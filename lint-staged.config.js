fac894556bb2b5bb30b58af137e5d60e780bf293
Arttu Hanska <arttu.hanska@gmail.com>
2020-09-27 21:24:57 +0300

    Fix lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,24 @@
 module.exports = {
   '*.{js,ts,tsx}': (files) => {
     const filenames = files.join(' ');
+
+    let clientFiles = [];
+    let serverFiles = [];
+    let sharedFiles = [];
+
+    files.map((file) => {
+      if (file.indexOf('client/') !== -1) clientFiles.push(file);
+      else if (file.indexOf('server/') !== -1) serverFiles.push(file);
+      else sharedFiles.push(file);
+    });
+
     return [
       `prettier --check ${filenames}`,
-      /*
-      files.length > 10 ? 'eslint .' : `eslint ${filenames}`,
-      `stylelint ${filenames}`,
-      `jest --bail --findRelatedTests ${filenames}`,
-      */
+      `eslint -c client/.eslintrc.js ${clientFiles}`,
+      `eslint -c server/.eslintrc.js ${serverFiles}`,
+      // `eslint -c server/.eslintrc.js ${sharedFiles}`,
+      // `stylelint ${filenames}`,
+      // `jest --bail --findRelatedTests ${filenames}`,
     ];
   },
 
