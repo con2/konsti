64ad0851916b626d7cfa4e337ccba6207dde7bb6
Arttu Hanska <arttu.hanska@gmail.com>
2021-01-31 19:23:14 +0200

    Run Cypress test in CI pipeline (#23)

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,13 +1,10 @@
 import { Config } from './typings/config.typings';
 
-const settings = typeof SETTINGS !== 'undefined' ? SETTINGS : 'development';
-
-const commonConfig = {
+export const config: Config = {
   // App info
   appName: 'Konsti',
 
   // App settings
-  loadedSettings: settings,
   SIGNUP_END_TIME: 30, // minutes
   SIGNUP_OPEN_TIME: 4, // hours
   MESSAGE_DELAY: 3000, // ms
@@ -26,33 +23,10 @@ const commonConfig = {
   reduxTrace: false,
   enableAxe: false,
   enableWhyDidYouRender: false,
-};
 
-const prodConfig = {
-  apiServerUrl: 'https://ropekonsti.fi',
-  useTestTime: false,
-  dataUpdateInterval: 60, // seconds
+  // Environment dependent
+  loadedSettings: process.env.SETTINGS ?? 'development',
+  apiServerUrl: process.env.API_SERVER_URL ?? 'http://localhost:5000',
+  useTestTime: process.env.USE_TEST_TIME === 'true' || false,
+  dataUpdateInterval: Number(process.env.DATA_UPDATE_INTERVAL) ?? 60, // seconds
 };
-
-const stagingConfig = {
-  apiServerUrl: 'https://test.ropekonsti.fi',
-  useTestTime: true,
-  dataUpdateInterval: 60, // seconds
-};
-
-const devConfig = {
-  apiServerUrl: 'http://localhost:5000',
-  useTestTime: true,
-  dataUpdateInterval: 60, // seconds
-};
-
-const combineConfig = (): Config => {
-  if (settings === 'production') {
-    return { ...commonConfig, ...prodConfig };
-  } else if (settings === 'staging') {
-    return { ...commonConfig, ...stagingConfig };
-  }
-  return { ...commonConfig, ...devConfig };
-};
-
-export const config: Config = combineConfig();
