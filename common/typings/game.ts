64ffc10a9f8b3aef3112856400f9fb51f06be093
Arttu Hanska <arttu.hanska@gmail.com>
2021-02-07 21:03:11 +0200

    Remove commented code

--- common/typings/game.ts
+++ common/typings/game.ts
@@ -1,34 +1,5 @@
 import { Record, String, Number, Static, Boolean, Array } from 'runtypes';
 
-/*
-export interface Game {
-  gameId: string;
-  title: string;
-  description: string;
-  location: string;
-  startTime: string;
-  mins: number;
-  tags: readonly string[];
-  genres: readonly string[];
-  styles: readonly string[];
-  language: string;
-  endTime: string;
-  people: string;
-  minAttendance: number;
-  maxAttendance: number;
-  gameSystem: string;
-  englishOk: boolean;
-  childrenFriendly: boolean;
-  ageRestricted: boolean;
-  beginnerFriendly: boolean;
-  intendedForExperiencedParticipants: boolean;
-  shortDescription: string;
-  revolvingDoor: boolean;
-  popularity: number;
-  programType: string;
-}
-*/
-
 export const GameRuntype = Record({
   gameId: String,
   title: String,
