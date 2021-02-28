a60919103d29ddaaf8c4e1bf08c76fa868cd332b
Arttu Hanska <arttu.hanska@gmail.com>
2021-02-28 20:30:02 +0200

    Fix import

--- client/src/test/setupTests.ts
+++ client/src/test/setupTests.ts
@@ -1,6 +1,6 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'regenerator-runtime/runtime';
-import 'test/i18nTest';
+import 'client/test/i18nTest';
 
 configure({ adapter: new Adapter() });
