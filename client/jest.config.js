818ae10b592d72b903e08e8be7664958a1d01c6a
Arttu Hanska <arttu.hanska@gmail.com>
2021-02-28 17:59:06 +0200

    Cleanup comments

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,30 +1,16 @@
 module.exports = {
-  // Automatically clear mock calls and instances between every test
   clearMocks: true,
-
-  // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
-
-  // A map from regular expressions to module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '\\.(jpg|png|gif|svg)$': '<rootDir>/src/test/__mocks__/binaryMock.ts',
   },
-
-  // The paths to modules that run some code to configure or set up the testing environment before each test
   setupFiles: ['./src/test/setupTests.ts'],
-
-  // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   snapshotSerializers: ['enzyme-to-json/serializer'],
-
-  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: ['cypress'],
-
-  // A map from regular expressions to paths to transformers
   transform: {
     '^.+\\.tsx?$': ['babel-jest', { rootMode: 'upward' }],
     '^.+\\.ts?$': ['babel-jest', { rootMode: 'upward' }],
     '^.+\\.js?$': ['babel-jest', { rootMode: 'upward' }],
   },
-
   modulePaths: [`<rootDir>/src`],
 };
--- server/jest.config.js
+++ server/jest.config.js
@@ -1,27 +1,13 @@
 module.exports = {
-  // Automatically clear mock calls and instances between every test
   clearMocks: true,
-
-  // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
-
-  // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   modulePathIgnorePatterns: ['<rootDir>/lib'],
-
-  // The paths to modules that run some code to configure or set up the testing environment before each test
   setupFiles: ['./src/test/setupTests.ts'],
-
-  // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: ['./src/test/setupTestsAfterEnv.ts'],
-
-  // The test environment that will be used for testing
   testEnvironment: 'node',
-
-  // A map from regular expressions to paths to transformers
   transform: {
     '^.+\\.ts?$': ['babel-jest', { rootMode: 'upward' }],
     '^.+\\.js?$': ['babel-jest', { rootMode: 'upward' }],
   },
-
   modulePaths: [`<rootDir>/src`],
 };
