afe56577bbfca49fa766beb3bbc62cefbed347de
Arttu Hanska <arttu.hanska@gmail.com>
2021-03-07 21:05:29 +0200

    Fix type errors

--- server/src/utils/closeServer.ts
+++ server/src/utils/closeServer.ts
@@ -3,12 +3,12 @@ import { db } from 'server/db/mongodb';
 import { logger } from 'server/utils/logger';
 
 export const closeServer = async (
-  server: Server,
+  server: Server | null,
   dbConnString: string
 ): Promise<void> => {
   try {
     await db.gracefulExit(dbConnString);
-    await server.close();
+    server && (await server.close());
   } catch (error) {
     logger.error(error);
   }
