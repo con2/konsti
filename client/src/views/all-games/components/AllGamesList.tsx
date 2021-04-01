9d246cb4195f185f2404b25f6ee9cd5ccce743ab
Niko Kauppila <niko.kauppila@iki.fi>
2021-04-01 11:22:09 +0300

    Show signed game count on timeslot header

--- client/src/views/all-games/components/AllGamesList.tsx
+++ client/src/views/all-games/components/AllGamesList.tsx
@@ -5,6 +5,9 @@ import styled from 'styled-components';
 import { timeFormatter } from 'client/utils/timeFormatter';
 import { Game } from 'shared/typings/models/game';
 import { GameEntry } from './GameEntry';
+import { Signup } from 'typings/user.typings';
+import { useSelector } from 'react-redux';
+import { RootState } from 'typings/redux.typings';
 
 export interface Props {
   games: readonly Game[];
@@ -13,6 +16,10 @@ export interface Props {
 export const AllGamesList: FC<Props> = (props: Props): ReactElement => {
   const { games } = props;
   const { t } = useTranslation();
+
+  const signedGames: readonly Signup[] = useSelector(
+    (state: RootState) => state.myGames.signedGames
+  );
 
   const buildGamesList = (games: readonly Game[]): ReactElement[] => {
     const sortedGames = _.sortBy(games, [
@@ -33,6 +40,9 @@ export const AllGamesList: FC<Props> = (props: Props): ReactElement => {
       const signupEndTime = timeFormatter.endTime(startTime);
 
       const allGamesRevolvingDoor = games.every((game) => game.revolvingDoor);
+      const signedGamesCount = signedGames.filter(
+        (game) => game.gameDetails.startTime === startTime
+      ).length;
 
       const title = (
         <GameListTitle key={startTime}>
@@ -43,13 +53,14 @@ export const AllGamesList: FC<Props> = (props: Props): ReactElement => {
               ({t('signupOpenBetween')} {signupStartTime}-{signupEndTime})
             </span>
           )}
+          <SignupCount>{signedGamesCount} / 3</SignupCount>
         </GameListTitle>
       );
 
       GamesList.push(title);
 
       const gameEntries = games.map((game) => (
-        <GameEntry key={game.gameId} game={game} />
+        <GameEntry key={game.gameId} game={game} startTime={startTime} />
       ));
       GamesList.push(...gameEntries);
     }
@@ -66,6 +77,10 @@ export const AllGamesList: FC<Props> = (props: Props): ReactElement => {
     </div>
   );
 };
+
+const SignupCount = styled.span`
+  float: right;
+`;
 
 const GameListTitle = styled.h3`
   margin: 20px 0;
