381d2855ddb49ebb5f9ab504261b6a1d3211c8a3
Arttu Hanska <arttu.hanska@gmail.com>
2021-04-05 15:18:20 +0300

    Import shared folder from jest tests (#51)

--- client/jest.config.js
+++ client/jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|png|gif|svg)$': '<rootDir>/src/test/__mocks__/binaryMock.ts',
     '^client(.*)$': '<rootDir>/src/$1',
+    '^shared(.*)$': '<rootDir>/../shared/$1',
   },
 
   setupFiles: ['./src/test/setupTests.ts'],
