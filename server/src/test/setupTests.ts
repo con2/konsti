00f4958784b8e4a87735b994cc6258c323ca36b6
Arttu Hanska <arttu.hanska@gmail.com>
2021-04-05 20:50:43 +0300

    Fail test if error is logged (#54)

--- server/src/test/setupTests.ts
+++ server/src/test/setupTests.ts
@@ -4,3 +4,8 @@ import { logger } from 'server/utils/logger';
 // Don't show info or debug logging in tests
 logger.info = jest.fn();
 logger.debug = jest.fn();
+
+// Throw if errors are logged
+logger.error = jest.fn().mockImplementation((message) => {
+  throw new Error(message);
+});
