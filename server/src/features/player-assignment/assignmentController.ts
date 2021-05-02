c4f41590161fbb4958f4c07c87289591879c878b
Arttu Hanska <arttu.hanska@gmail.com>
2021-05-02 21:38:22 +0300

    Add shared config folder

--- server/src/features/player-assignment/assignmentController.ts
+++ server/src/features/player-assignment/assignmentController.ts
@@ -6,7 +6,7 @@ import { config } from 'server/config';
 import { ASSIGNMENT_ENDPOINT } from 'shared/constants/apiEndpoints';
 import { PostPlayerAssignmentResponse } from 'shared/typings/api/assignment';
 import { ServerError } from 'shared/typings/api/errors';
-import { sharedConfig } from 'shared/sharedConfig';
+import { sharedConfig } from 'shared/config/sharedConfig';
 
 // Assign players to games
 export const storeAssignment = async (
