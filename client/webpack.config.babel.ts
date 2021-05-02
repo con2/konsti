2c887ccb2d69664992d84240420163f897d0ab48
Arttu Hanska <arttu.hanska@gmail.com>
2021-05-02 21:20:07 +0300

    Webpack: use absolute path to client config

--- client/webpack.config.babel.ts
+++ client/webpack.config.babel.ts
@@ -7,7 +7,7 @@ import Dotenv from 'dotenv-webpack';
 import { Configuration } from 'webpack';
 import { merge } from 'webpack-merge';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
-import { config } from './src/config';
+import { config } from 'client/config';
 import { sharedConfig } from 'shared/sharedConfig';
 
 const TARGET = process.env.npm_lifecycle_event;
