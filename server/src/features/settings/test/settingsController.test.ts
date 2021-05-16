7b16b9027d9577d8983bad579db0ed2e2dec1129
Arttu Hanska <arttu.hanska@gmail.com>
2021-05-16 18:12:47 +0300

    Rename and combine test files (#86)

--- server/src/features/settings/test/settingsController.test.ts
+++ server/src/features/settings/test/settingsController.test.ts
@@ -3,7 +3,12 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import { Application } from 'express';
 import { startServer } from 'server/utils/startServer';
 import { closeServer } from 'server/utils/closeServer';
-import { SETTINGS_ENDPOINT } from 'shared/constants/apiEndpoints';
+import {
+  HIDDEN_ENDPOINT,
+  SETTINGS_ENDPOINT,
+  SIGNUPTIME_ENDPOINT,
+  TOGGLE_APP_OPEN_ENDPOINT,
+} from 'shared/constants/apiEndpoints';
 
 let server: Application;
 let mongoServer: MongoMemoryServer;
@@ -24,5 +29,26 @@ describe(`GET ${SETTINGS_ENDPOINT}`, () => {
   test('should return 200', async () => {
     const response = await request(server).get(SETTINGS_ENDPOINT);
     expect(response.status).toEqual(200);
+  });
+});
+
+describe(`POST ${HIDDEN_ENDPOINT}`, () => {
+  test('should return 401 without valid authorization', async () => {
+    const response = await request(server).post(HIDDEN_ENDPOINT);
+    expect(response.status).toEqual(401);
+  });
+});
+
+describe(`POST ${SIGNUPTIME_ENDPOINT}`, () => {
+  test('should return 401 without valid authorization', async () => {
+    const response = await request(server).post(SIGNUPTIME_ENDPOINT);
+    expect(response.status).toEqual(401);
+  });
+});
+
+describe(`POST ${TOGGLE_APP_OPEN_ENDPOINT}`, () => {
+  test('should return 401 without valid authorization', async () => {
+    const response = await request(server).post(TOGGLE_APP_OPEN_ENDPOINT);
+    expect(response.status).toEqual(401);
   });
 });
