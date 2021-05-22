5c8b48bf25ff6d176b738d66c18b65eeffe2f44f
niko <niko.kauppila@iki.fi>
2021-05-22 16:48:04 +0300

    Implement support for directly entering games

--- client/src/services/signupServices.ts
+++ client/src/services/signupServices.ts
@@ -1,8 +1,18 @@
 import { AxiosResponse, AxiosError } from 'axios';
 import { api } from 'client/utils/api';
 import { ServerError } from 'shared/typings/api/errors';
-import { SIGNUP_ENDPOINT } from 'shared/constants/apiEndpoints';
-import { PostSignupResponse, SignupData } from 'shared/typings/api/signup';
+import {
+  ENTERED_GAME_ENDPOINT,
+  SIGNUP_ENDPOINT,
+} from 'shared/constants/apiEndpoints';
+import {
+  DeleteEnteredGameParameters,
+  DeleteEnteredGameResponse,
+  PostEnteredGameParameters,
+  PostEnteredGameResponse,
+  PostSignupResponse,
+  SignupData,
+} from 'shared/typings/api/signup';
 
 export const postSignup = async (
   signupData: SignupData
@@ -12,6 +22,46 @@ export const postSignup = async (
     response = await api.post<PostSignupResponse>(SIGNUP_ENDPOINT, {
       signupData,
     });
+  } catch (error) {
+    if (error?.response) {
+      const axiosError: AxiosError<ServerError> = error;
+      if (axiosError.response) return axiosError.response.data;
+    }
+    throw error;
+  }
+
+  return response.data;
+};
+
+export const postEnteredGame = async (
+  requestData: PostEnteredGameParameters
+): Promise<PostEnteredGameResponse | ServerError> => {
+  let response: AxiosResponse;
+  try {
+    response = await api.post<PostEnteredGameParameters>(
+      ENTERED_GAME_ENDPOINT,
+      requestData
+    );
+  } catch (error) {
+    if (error?.response) {
+      const axiosError: AxiosError<ServerError> = error;
+      if (axiosError.response) return axiosError.response.data;
+    }
+    throw error;
+  }
+
+  return response.data;
+};
+
+export const deleteEnteredGame = async (
+  requestData: DeleteEnteredGameParameters
+): Promise<DeleteEnteredGameResponse | ServerError> => {
+  let response: AxiosResponse;
+  try {
+    response = await api.delete<DeleteEnteredGameParameters>(
+      ENTERED_GAME_ENDPOINT,
+      { data: requestData }
+    );
   } catch (error) {
     if (error?.response) {
       const axiosError: AxiosError<ServerError> = error;
