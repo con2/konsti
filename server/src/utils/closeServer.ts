5450239708b9b0fa4d1c43f04d2774dffb324450
Arttu Hanska <arttu.hanska@gmail.com>
2021-06-08 20:55:34 +0300

    Don't pass connString to db.close() (#108)

--- server/src/utils/closeServer.ts
+++ server/src/utils/closeServer.ts
@@ -2,13 +2,10 @@ import { Server } from 'http';
 import { db } from 'server/db/mongodb';
 import { logger } from 'server/utils/logger';
 
-export const closeServer = async (
-  server: Server,
-  dbConnString: string
-): Promise<void> => {
+export const closeServer = async (server: Server): Promise<void> => {
   try {
-    await db.gracefulExit(dbConnString);
-    server && (await server.close());
+    await db.gracefulExit();
+    await server.close();
   } catch (error) {
     logger.error(error);
   }
