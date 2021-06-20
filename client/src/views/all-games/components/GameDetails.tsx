58c602c24f8a92a8d3c9c27ac36d800b43ba6c4b
Arttu Hanska <arttu.hanska@gmail.com>
2021-06-20 21:40:51 +0300

    Allow saving signup messages

--- client/src/views/all-games/components/GameDetails.tsx
+++ client/src/views/all-games/components/GameDetails.tsx
@@ -1,7 +1,11 @@
-import React, { ReactElement, useState, useEffect } from 'react';
+import React, { ReactElement, useState, useEffect, ChangeEvent } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useHistory, useParams } from 'react-router-dom';
-import { submitUpdateHidden } from 'client/views/admin/adminThunks';
+import {
+  submitAddSignupMessage,
+  submitDeleteSignupMessage,
+  submitUpdateHidden,
+} from 'client/views/admin/adminThunks';
 import { FeedbackForm } from 'client/views/all-games/components/FeedbackForm';
 import { GameInfo } from 'client/views/all-games/components/GameInfo';
 import { Loading } from 'client/components/Loading';
@@ -9,6 +13,7 @@ import { Game } from 'shared/typings/models/game';
 import { updateFavorite, UpdateFavoriteOpts } from 'client/utils/favorite';
 import { useAppDispatch, useAppSelector } from 'client/utils/hooks';
 import { Button } from 'client/components/Button';
+import styled from 'styled-components';
 
 export const GameDetails = (): ReactElement => {
   const history = useHistory();
@@ -23,6 +28,7 @@ export const GameDetails = (): ReactElement => {
     (state) => state.myGames.favoritedGames
   );
   const hiddenGames = useAppSelector((state) => state.admin.hiddenGames);
+  const signupMessages = useAppSelector((state) => state.admin.signupMessages);
   const dispatch = useAppDispatch();
   const { t } = useTranslation();
 
@@ -30,8 +36,13 @@ export const GameDetails = (): ReactElement => {
 
   const [hidden, setHidden] = useState<boolean>(false);
   const [favorited, setFavorited] = useState<boolean>(false);
+  const [hasSignupMessage, setHasSignupMessage] = useState<boolean>(false);
   const [submitting, setSubmitting] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(true);
+
+  const [signupMessageInput, setSignupMessageInput] = useState<string>('');
+  const [signupMessageInputVisible, setSignupMessageInputVisible] =
+    useState<boolean>(false);
 
   useEffect(() => {
     setLoading(true);
@@ -52,11 +63,18 @@ export const GameDetails = (): ReactElement => {
           setHidden(true);
         }
       });
+
+      // Check if signup message exinsts
+      signupMessages.find((signupMessageForGame) => {
+        if (signupMessageForGame.gameId === foundGame.gameId) {
+          setHasSignupMessage(true);
+        }
+      });
     };
 
     checkGameState();
     setLoading(false);
-  }, [foundGame, favoritedGames, hiddenGames]);
+  }, [foundGame, favoritedGames, hiddenGames, signupMessages]);
 
   // Favorite / remove favorite clicked
   const updateFavoriteHandler = async (action: string): Promise<void> => {
@@ -115,6 +133,30 @@ export const GameDetails = (): ReactElement => {
     }
   };
 
+  const handleSignupMessageChange = (
+    event: ChangeEvent<HTMLInputElement>
+  ): void => {
+    setSignupMessageInput(event.target.value);
+  };
+
+  const onSubmitAddSignupMessageClick = (): void => {
+    if (!foundGame) return;
+    dispatch(
+      submitAddSignupMessage({
+        gameId: foundGame.gameId,
+        message: signupMessageInput,
+      })
+    );
+    setSignupMessageInputVisible(false);
+    setSignupMessageInput('');
+  };
+
+  const submitRemoveSignupMessage = (): void => {
+    if (!foundGame) return;
+    dispatch(submitDeleteSignupMessage(foundGame.gameId));
+    setHasSignupMessage(false);
+  };
+
   return (
     <div className='game-details-view'>
       <div className='details-button-row'>
@@ -165,9 +207,60 @@ export const GameDetails = (): ReactElement => {
             {t('button.hide')}
           </Button>
         )}
+
+        {!hasSignupMessage &&
+          !signupMessageInputVisible &&
+          loggedIn &&
+          userGroup === 'admin' &&
+          foundGame && (
+            <Button
+              disabled={submitting}
+              onClick={() => setSignupMessageInputVisible(true)}
+            >
+              {t('button.addSignupMessage')}
+            </Button>
+          )}
+
+        {!hasSignupMessage &&
+          signupMessageInputVisible &&
+          loggedIn &&
+          userGroup === 'admin' &&
+          foundGame && (
+            <Button
+              disabled={submitting}
+              onClick={() => setSignupMessageInputVisible(false)}
+            >
+              {t('button.cancel')}
+            </Button>
+          )}
+
+        {hasSignupMessage && loggedIn && userGroup === 'admin' && foundGame && (
+          <Button
+            disabled={submitting}
+            onClick={() => submitRemoveSignupMessage()}
+          >
+            {t('button.removeSignupMessage')}
+          </Button>
+        )}
       </div>
 
       {loading && <Loading />}
+
+      {signupMessageInputVisible && (
+        <>
+          <p>{t('gameDetails.addSignupTextField')}</p>
+          <FormInput
+            type={'text'}
+            key='new-password'
+            placeholder={t('gameDetails.addSignupTextField')}
+            value={signupMessageInput}
+            onChange={handleSignupMessageChange}
+          />
+          <Button onClick={onSubmitAddSignupMessageClick}>
+            {t('button.save')}
+          </Button>
+        </>
+      )}
 
       {!loading && !foundGame && (
         <div className='game-not-found'>
@@ -194,3 +287,11 @@ const findGame = (gameId: string, array: readonly Game[]): number => {
   }
   return -1;
 };
+
+const FormInput = styled.input`
+  border: 1px solid ${(props) => props.theme.borderInactive};
+  color: ${(props) => props.theme.buttonText};
+  height: 34px;
+  padding: 0 0 0 10px;
+  width: 100%;
+`;
