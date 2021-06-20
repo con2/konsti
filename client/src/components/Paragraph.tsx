4aa38d2ad4b573eb9521ec29d78d82baf4f1f989
Arttu Hanska <arttu.hanska@gmail.com>
2021-06-20 18:41:07 +0300

    Refactor accordion component (#128)

---
+++ client/src/components/Paragraph.tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement } from 'react';
+
+interface Props {
+  text: string;
+}
+
+export const Paragraph = ({ text }: Props): ReactElement => {
+  const rows = text.split('\n');
+  return (
+    <>
+      {rows.map((row) => (
+        <p key={row}>{row}</p>
+      ))}
+    </>
+  );
+};
