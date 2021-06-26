9d82bd60deb2064b5c24e1b3f4448cf619afd3cf
Arttu Hanska <arttu.hanska@gmail.com>
2021-06-26 13:47:42 +0300

    Add signup messages to admin view (#133)

--- client/src/views/admin/components/HiddenGamesList.test.tsx
+++ client/src/views/admin/components/HiddenGamesList.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { Hidden, Props } from 'client/views/admin/components/Hidden';
+import {
+  HiddenGamesList,
+  Props,
+} from 'client/views/admin/components/HiddenGamesList';
 
 test('should render correctly', () => {
   const props: Props = { hiddenGames: [] };
 
-  render(<Hidden {...props} />);
+  render(<HiddenGamesList {...props} />);
 });
