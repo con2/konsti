e46d9730ceb134348b03916ef8d92a723698629a
Arttu Hanska <arttu.hanska@gmail.com>
2021-06-28 11:15:20 +0300

    Add toggle to show and hide signup messages (#139)

--- client/src/utils/icons.ts
+++ client/src/utils/icons.ts
@@ -1,24 +1,30 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
-  faAngleUp,
-  faAngleDown,
-  faEye,
-  faEyeSlash,
-  faHeart,
-  faBars,
-  faTimes,
+  faAngleUp as fasAngleUp,
+  faAngleDown as fasAngleDown,
+  faEye as fasEye,
+  faEyeSlash as fasEyeSlash,
+  faHeart as fasHeart,
+  faBars as fasBars,
+  faTimes as fasTimes,
+  faComment as fasComment,
 } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
+import {
+  faHeart as farHeart,
+  faComment as farComment,
+} from '@fortawesome/free-regular-svg-icons';
 
 export const getIconLibrary = (): void => {
   library.add(
-    faAngleUp,
-    faAngleDown,
-    faEye,
-    faEyeSlash,
-    faHeart,
-    far,
-    faBars,
-    faTimes
+    fasAngleUp,
+    fasAngleDown,
+    fasEye,
+    fasEyeSlash,
+    fasHeart,
+    farHeart,
+    fasBars,
+    fasTimes,
+    farComment,
+    fasComment
   );
 };
