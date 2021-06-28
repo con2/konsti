694dacd48571d7204950062744eb7917789a595f
Arttu Hanska <arttu.hanska@gmail.com>
2021-06-28 18:49:31 +0300

    Add value too long and too short messages to registration form (#147)

--- client/src/utils/validate.ts
+++ client/src/utils/validate.ts
@@ -1,3 +1,20 @@
+import {
+  PASSWORD_LENGTH_MAX,
+  PASSWORD_LENGTH_MIN,
+  USERNAME_LENGTH_MAX,
+  USERNAME_LENGTH_MIN,
+} from 'shared/constants/validation';
+
 export const required = (value: string): string | undefined => {
   if (!value) return 'validation.required';
+};
+
+export const usernameLength = (value: string): string | undefined => {
+  if (value.length < USERNAME_LENGTH_MIN) return 'validation.tooShort';
+  if (value.length > USERNAME_LENGTH_MAX) return 'validation.tooLong';
+};
+
+export const passwordLength = (value: string): string | undefined => {
+  if (value.length < PASSWORD_LENGTH_MIN) return 'validation.tooShort';
+  if (value.length > PASSWORD_LENGTH_MAX) return 'validation.tooLong';
 };
--- server/src/api/validation.ts
+++ server/src/api/validation.ts
@@ -1,15 +1,33 @@
 import { check, ValidationChain } from 'express-validator';
 import { sharedConfig } from 'shared/config/sharedConfig';
 import { ConventionType } from 'shared/config/sharedConfig.types';
+import {
+  PASSWORD_LENGTH_MAX,
+  PASSWORD_LENGTH_MIN,
+  USERNAME_LENGTH_MAX,
+  USERNAME_LENGTH_MIN,
+} from 'shared/constants/validation';
 
 export const postUserValidation = (): ValidationChain[] => {
   const validation = [
-    check('username').not().isEmpty().isLength({ max: 20 }).trim().escape(),
-    check('password').not().isEmpty().trim().escape(),
+    check('username')
+      .not()
+      .isEmpty()
+      .isLength({ min: USERNAME_LENGTH_MIN, max: USERNAME_LENGTH_MAX })
+      .trim()
+      .escape(),
+    check('password')
+      .not()
+      .isEmpty()
+      .isLength({ min: PASSWORD_LENGTH_MIN, max: PASSWORD_LENGTH_MAX })
+      .trim()
+      .escape(),
   ];
+
   if (sharedConfig.conventionType === ConventionType.LIVE) {
     validation.push(check('serial').not().isEmpty().trim().escape());
   }
+
   return validation;
 };
 
