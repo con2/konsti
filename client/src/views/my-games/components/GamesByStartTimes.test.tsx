9a6cef9dd476edb8e2e598f34d0506c8f6a8d16a
Olli-Pekka Tikkasalo <optikkanen@gmail.com>
2021-07-13 19:35:13 +0300

    Added cancel button to my games (#174)
    * Added cancel button to my games
    
    * Fixed test for ResultsByStartTiems
    
    * Added remove favorite button to MyGames
    
    * Added husky / _ / folder to gitignore
    
    * Added .vscode to .prettierignore

--- client/src/views/my-games/components/GamesByStartTimes.test.tsx
+++ client/src/views/my-games/components/GamesByStartTimes.test.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from 'client/utils/store';
 import {
   GamesByStartTimes,
   Props,
 } from 'client/views/my-games/components/GamesByStartTimes';
 
-test('should render correctly', () => {
+test('should render correctly', async () => {
   const props: Props = { games: [], startTimes: [] };
-
-  render(<GamesByStartTimes {...props} />);
+  await act(async () => {
+    await render(
+      <Provider store={store}>
+        <GamesByStartTimes {...props} />
+      </Provider>
+    );
+  });
 });
