ddba5df105d729919a3826920ade3d376d64f372
Olli-Pekka Tikkasalo <optikkanen@gmail.com>
2021-07-23 13:20:15 +0300

    Cancel dialog to cancelling signups in all-games and my-games (#188)
    * Added cancel dialog to all-games
    
    * Added cancel dialog to my-games
    
    * Added cancel dialog to my-games
    
    * Set cancel signup button color
    
    * Fixed eslint errors

---
+++ client/src/views/all-games/components/CancelSignupForm.tsx
@@ -0,0 +1,69 @@
+import React, { FC, ReactElement, FormEvent } from 'react';
+import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
+import { Game } from 'shared/typings/models/game';
+import { useAppDispatch, useAppSelector } from 'client/utils/hooks';
+import { Button } from 'client/components/Button';
+import { submitDeleteGame } from 'client/views/signup/signupThunks';
+
+interface Props {
+  game: Game;
+  onCancelSignup: () => void;
+  onCancelForm: () => void;
+}
+
+export const CancelSignupForm: FC<Props> = (props: Props): ReactElement => {
+  const { game, onCancelSignup, onCancelForm } = props;
+
+  const { t } = useTranslation();
+  const dispatch = useAppDispatch();
+  const username = useAppSelector((state) => state.login.username);
+
+  const handleCancel = (): void => {
+    onCancelForm();
+  };
+  const removeSignup = async (event: FormEvent): Promise<void> => {
+    await dispatch(
+      submitDeleteGame({
+        username,
+        startTime: game.startTime,
+        enteredGameId: game.gameId,
+      })
+    );
+    onCancelSignup();
+  };
+
+  return (
+    <ButtonContainer>
+      <CancelSignupButton onClick={removeSignup}>
+        {t('signup.confirmCancelSignup')}
+      </CancelSignupButton>
+      <CancelFormButton onClick={handleCancel}>
+        {t('signup.cancel')}
+      </CancelFormButton>
+    </ButtonContainer>
+  );
+};
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const CancelSignupButton = styled(Button)`
+  width: 50%;
+  background: ${(props) => props.theme.buttonCancelSignup};
+  border: 1px solid ${(props) => props.theme.buttonCancelSignupBorder};
+  color: ${(props) => props.theme.mainText};
+  &:hover,
+  &:focus {
+    border: 1px solid ${(props) => props.theme.buttonCancelSignupBorder};
+    background: ${(props) => props.theme.buttonCancelSignupHover};
+    color: ${(props) => props.theme.mainText};
+  }
+`;
+
+const CancelFormButton = styled(Button)`
+  width: 50%;
+  border: 1px solid ${(props) => props.theme.informative};
+`;
---
+++ client/src/views/my-games/components/CancelSignupForm.tsx
@@ -0,0 +1,62 @@
+import React, { FC, ReactElement } from 'react';
+import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'client/components/Button';
+import { Game } from 'shared/typings/models/game';
+import { submitDeleteGame } from 'client/views/signup/signupThunks';
+import { useAppDispatch, useAppSelector } from 'client/utils/hooks';
+
+interface Props {
+  game: Game;
+  onCancelSignup: () => void;
+  onCancelForm: () => void;
+}
+
+export const CancelSignupForm: FC<Props> = (props: Props): ReactElement => {
+  const { game, onCancelSignup, onCancelForm } = props;
+  const { t } = useTranslation();
+  const dispatch = useAppDispatch();
+  const username = useAppSelector((state) => state.login.username);
+
+  const handleCancel = (): void => {
+    onCancelForm();
+  };
+
+  const removeSignup = async (): Promise<void> => {
+    await dispatch(
+      submitDeleteGame({
+        username,
+        startTime: game.startTime,
+        enteredGameId: game.gameId,
+      })
+    );
+    onCancelSignup();
+  };
+
+  return (
+    <>
+      <CancelSignupButton onClick={removeSignup}>
+        {t('signup.confirmCancelSignup')}
+      </CancelSignupButton>{' '}
+      <CancelFormButton onClick={handleCancel}>
+        {t('signup.cancel')}
+      </CancelFormButton>
+    </>
+  );
+};
+
+const CancelSignupButton = styled(Button)`
+  background: ${(props) => props.theme.buttonCancelSignup};
+  border: 1px solid ${(props) => props.theme.buttonCancelSignupBorder};
+  color: ${(props) => props.theme.mainText};
+  &:hover,
+  &:focus {
+    border: 1px solid ${(props) => props.theme.buttonCancelSignupBorder};
+    background: ${(props) => props.theme.buttonCancelSignupHover};
+    color: ${(props) => props.theme.mainText};
+  }
+`;
+
+const CancelFormButton = styled(Button)`
+  border: 1px solid ${(props) => props.theme.informative};
+`;
