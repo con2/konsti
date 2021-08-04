3f67b8d53c189a58fec775a22138607d2cc5d450
Arttu Hanska <arttu.hanska@gmail.com>
2021-08-04 17:45:47 +0300

    Replace redux-form with react-hook-form (#221)

--- client/src/views/registration/RegistrationView.tsx
+++ client/src/views/registration/RegistrationView.tsx
@@ -1,31 +1,29 @@
 import React, { ReactElement } from 'react';
-import { SubmissionError } from 'redux-form';
 import { useTranslation } from 'react-i18next';
-import { submitRegistration } from 'client/views/registration/registrationThunks';
-import RegistrationForm from 'client/views/registration/components/RegistrationForm';
-import { RegistrationFormFields } from 'shared/typings/api/login';
-import { useAppDispatch } from 'client/utils/hooks';
+import styled from 'styled-components';
+import { RegistrationForm } from 'client/views/registration/components/RegistrationForm';
 
 export const RegistrationView = (): ReactElement => {
-  const dispatch = useAppDispatch();
   const { t } = useTranslation();
-
-  const submit = async (
-    registrationFormFields: RegistrationFormFields
-  ): Promise<void> => {
-    try {
-      await dispatch(submitRegistration(registrationFormFields));
-    } catch (error) {
-      throw new SubmissionError({
-        _error: t(error.message),
-      });
-    }
-  };
 
   return (
     <div className='registration-view'>
-      {/* @ts-expect-error: Type '{}' is missing the following properties from type 'RegistrationFormFields': password, registerDescription, serial, username */}
-      <RegistrationForm onSubmit={submit} />
+      <NotificationMessage>
+        {t('pageTitle.usernamePublicWarning')}.{' '}
+        {t('pageTitle.usernameDiscordInfo')}
+        <p>
+          <a href={'https://2021.ropecon.fi/etaropecon/ohjeet/'}>
+            {t('discordHint')}
+          </a>
+        </p>
+      </NotificationMessage>
+
+      <RegistrationForm />
     </div>
   );
 };
+
+const NotificationMessage = styled.div`
+  color: ${(props) => props.theme.warning};
+  margin: 20px 0 0 0;
+`;
