39da44a9f7aa071cc0a3623c582f486c4c75197c
Arttu Hanska <arttu.hanska@gmail.com>
2021-08-04 17:45:47 +0300

    Replace redux-form with react-hook-form (#221)

--- client/src/views/login/LoginView.tsx
+++ client/src/views/login/LoginView.tsx
@@ -1,29 +1,21 @@
 import React, { ReactElement } from 'react';
 import { useTranslation } from 'react-i18next';
-import { SubmissionError } from 'redux-form';
-import { submitLogin } from 'client/views/login/loginThunks';
-import LoginForm from 'client/views/login/components/LoginForm';
-import { LoginFormFields } from 'shared/typings/api/login';
-import { useAppDispatch } from 'client/utils/hooks';
+import { LoginForm } from 'client/views/login/components/LoginForm';
 
 export const LoginView = (): ReactElement => {
-  const dispatch = useAppDispatch();
   const { t } = useTranslation();
-
-  const submit = async (loginFormFields: LoginFormFields): Promise<void> => {
-    try {
-      await dispatch(submitLogin(loginFormFields));
-    } catch (error) {
-      throw new SubmissionError({
-        _error: t(error.message),
-      });
-    }
-  };
 
   return (
     <div className='login-view'>
-      {/* @ts-expect-error: Type '{}' is missing the following properties from type 'LoginFormFields': username, password */}
-      <LoginForm onSubmit={submit} />
+      <h2>{t('pageTitle.login')}</h2>
+
+      <LoginForm />
+
+      <p>{t('login.passwordHint')}</p>
+
+      <a href={'https://2021.ropecon.fi/etaropecon/ohjeet/'}>
+        {t('discordHint')}
+      </a>
     </div>
   );
 };
