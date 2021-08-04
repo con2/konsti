278eac70a35ec0e23009a1e29f1f96b2443b04be
Arttu Hanska <arttu.hanska@gmail.com>
2021-08-04 17:11:07 +0300

    Replace redux-form with react-hook-form

--- client/src/views/login/components/LoginForm.test.tsx
+++ client/src/views/login/components/LoginForm.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
-import LoginForm from 'client/views/login/components/LoginForm';
+import { LoginForm } from 'client/views/login/components/LoginForm';
 import { store } from 'client/utils/store';
 
 test('should render correctly', () => {
--- client/src/views/registration/components/RegistrationForm.test.tsx
+++ client/src/views/registration/components/RegistrationForm.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
-import RegistrationForm from 'client/views/registration/components/RegistrationForm';
+import { RegistrationForm } from 'client/views/registration/components/RegistrationForm';
 import { store } from 'client/utils/store';
 
 test('should render correctly', () => {
