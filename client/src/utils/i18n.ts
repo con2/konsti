56c649748c0b92276f303d68c9b24b771d7e4511
Arttu Hanska <arttu.hanska@gmail.com>
2021-08-06 16:58:43 +0300

    Remove deprecated i18next-xhr-backend (#229)

--- client/src/utils/i18n.ts
+++ client/src/utils/i18n.ts
@@ -1,36 +1,26 @@
 import i18next from "i18next";
-import backend, { BackendOptions } from "i18next-xhr-backend";
 import languageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-import en from "client/locales/en.json";
-import fi from "client/locales/fi.json";
+import translationEN from "client/locales/en.json";
+import translationFI from "client/locales/fi.json";
 
-const loadLocales = (
-  url: string,
-  _options: BackendOptions,
-  callback: (locale: object, config: object) => void
-): void => {
-  if (url === "en") {
-    callback(en, { status: "200" });
-  } else if (url === "fi") {
-    callback(fi, { status: "200" });
-  }
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  fi: {
+    translation: translationFI,
+  },
 };
 
 i18next
-  .use(backend)
   .use(languageDetector)
   .use(initReactI18next)
   .init({
-    backend: {
-      loadPath: "{{lng}}",
-      parse: (data: string) => data,
-      ajax: loadLocales,
-    },
-    fallbackLng: "en",
-    debug: false,
+    resources,
+    fallbackLng: "en", // use if detected lng is not available
     interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+      escapeValue: false, // react escapes by default -> safe from xss
     },
     detection: {
       order: ["localStorage"],
