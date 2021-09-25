52f97594186a01e6288876e8263a3cf33fbec192
Arttu Hanska <arttu.hanska@gmail.com>
2021-09-25 14:26:20 +0300

    Replace Cypress with Playwright

---
+++ client/playwright/playwright.config.ts
@@ -0,0 +1,63 @@
+import { PlaywrightTestConfig, Project, devices } from "@playwright/test";
+
+const ENABLE_CHROME = true;
+const ENABLE_FIREFOX = false;
+const ENABLE_SAFARI = false;
+const ENABLE_MOBILE_CHROME = false;
+const ENABLE_MOBILE_SAFARI = false;
+
+const projects: Project[] = [];
+
+ENABLE_CHROME &&
+  projects.push({
+    name: "Chrome Stable",
+    use: {
+      browserName: "chromium",
+      // channel: "chrome", // Test against Chrome Stable channel.
+    },
+  });
+
+ENABLE_FIREFOX &&
+  projects.push({
+    name: "Desktop Firefox",
+    use: {
+      browserName: "firefox",
+      viewport: { width: 800, height: 600 },
+    },
+  });
+
+ENABLE_SAFARI &&
+  projects.push({
+    name: "Desktop Safari",
+    use: {
+      browserName: "webkit",
+      viewport: { width: 1200, height: 750 },
+    },
+  });
+
+ENABLE_MOBILE_CHROME &&
+  projects.push({
+    name: "Mobile Safari",
+    use: devices["iPhone 12"],
+  });
+
+ENABLE_MOBILE_SAFARI &&
+  projects.push({
+    name: "Mobile Chrome",
+    use: devices["Pixel 5"],
+  });
+
+const config: PlaywrightTestConfig = {
+  projects,
+  retries: 1,
+  outputDir: "./test-results",
+  use: {
+    baseURL: process.env.PLAYWRIGHT_BASEURL ?? "http://localhost:5000",
+    video: "on",
+    trace: "on-first-retry",
+    headless: true,
+  },
+};
+
+// eslint-disable-next-line import/no-unused-modules
+export default config;
