a3d401d9fc9db856056e58f6ac0dbb3c9b65782f
Altesmi <optikkanen@gmail.com>
2022-02-13 17:28:17 +0200

    Started adding pop text and button

--- client/src/views/all-games/components/GameEntry.tsx
+++ client/src/views/all-games/components/GameEntry.tsx
@@ -55,6 +55,15 @@ export const GameEntry = ({
     return `${hoursStr} ${minutesStr}`;
   };
 
+  const formatPopularity = (popularity: number): string => {
+    let popularityStr = 'green';
+    if (popularity > 4 && popularity < 6) {
+      popularityStr = 'yellow';
+    } else if (popularity >= 6) {
+      popularityStr = 'red';
+    }
+    return popularityStr
+  }
   // Favorite / remove favorite clicked
   const updateFavoriteHandler = async (
     updateOpts: UpdateFavoriteOpts
@@ -68,7 +77,7 @@ export const GameEntry = ({
     <GameContainer key={game.gameId}>
       <GameHeader>
         <HeaderContainer>
-          <h3>{game.title}</h3>
+          <h3><PopularityCircle color={formatPopularity(game.popularity)} /> {game.title}</h3>
           <p>
             {t("signup.expectedDuration", {
               EXPECTED_DURATION: formatDuration(game.mins),
@@ -118,6 +127,7 @@ export const GameEntry = ({
           <TagColumn>
             <Tag>{t(`programType.${game.programType}`)}</Tag>
             {game.gameSystem && <Tag>{game.gameSystem}</Tag>}
+            <Tag>{t('high-popularity')}</Tag>
           </TagColumn>
         </GameTags>
       </GameHeader>
@@ -227,4 +237,29 @@ const GameTags = styled.div`
 
 const FavoriteIcon = styled(FontAwesomeIcon)`
   color: ${(props) => props.theme.favorited};
+`;
+
+const Popularity = styled.span`
+display: flex;
+flex-direction: row;
+`;
+
+const PopularityCircle = styled.div`
+  height: 10px;
+  width: 10px;
+  background-color: ${(props) => props.color};
+  border: 1px solid black;
+  border-radius: 50%;
+`;
+
+const PopularityInfo = styled.p`
+  min-width: 120px;
+  height: 14px;
+  text-align: left;
+  border-radius: 4px;
+  background: grey;
+  padding: 4px;
+  margin-bottom: 4px;
+  font-size: 12px;
+  color: teal;
 `;
