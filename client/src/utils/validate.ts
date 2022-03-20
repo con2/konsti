3f527eb3c6f84795c9bea3ede79095883fc58d32
Arttu Hanska <arttu.hanska@gmail.com>
2022-03-20 20:27:28 +0200

    Fix ChangePasswordForm min/max value in error

--- client/src/utils/validate.ts
+++ client/src/utils/validate.ts
@@ -1,9 +1,12 @@
+import { t } from "i18next";
 import {
   PASSWORD_LENGTH_MAX,
   PASSWORD_LENGTH_MIN,
 } from "shared/constants/validation";
 
 export const passwordLength = (value: string): string | undefined => {
-  if (value.length < PASSWORD_LENGTH_MIN) return "validation.tooShort";
-  if (value.length > PASSWORD_LENGTH_MAX) return "validation.tooLong";
+  if (value.length < PASSWORD_LENGTH_MIN)
+    return t("validation.tooShort", { length: PASSWORD_LENGTH_MIN });
+  if (value.length > PASSWORD_LENGTH_MAX)
+    return t("validation.tooLong", { length: PASSWORD_LENGTH_MAX });
 };
