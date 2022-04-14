c7ff545a30220846cf0cd54464c885519530609e
Arttu Hanska <arttu.hanska@gmail.com>
2022-04-14 21:14:32 +0300

    Add tests for Kompassi update

--- server/src/features/player-assignment/utils/removeOverlapSignups.test.ts
+++ server/src/features/player-assignment/utils/removeOverlapSignups.test.ts
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
-import { UserModel } from "server/features/user/userSchema";
-import { GameModel } from "server/features/game/gameSchema";
 import { removeOverlapSignups } from "server/features/player-assignment/utils/removeOverlapSignups";
 import { mockUser, mockSignup } from "server/test/mock-data/mockUser";
 import { mockResults } from "server/test/mock-data/mockResults";
 import { testGame, testGame2 } from "shared/tests/testGame";
-import { saveSignup, saveUser } from "server/features/user/userRepository";
-import { saveGames } from "server/features/game/gameRepository";
+import {
+  findUser,
+  saveSignup,
+  saveUser,
+} from "server/features/user/userRepository";
+import { findGames, saveGames } from "server/features/game/gameRepository";
 
 let mongoServer: MongoMemoryServer;
 
@@ -25,19 +27,16 @@ afterEach(async () => {
 
 test("should remove overlapping signups from user", async () => {
   await saveGames([testGame, testGame2]);
-  const insertedGames = await GameModel.find({});
+  const insertedGames = await findGames();
   expect(insertedGames.length).toEqual(2);
 
   await saveUser(mockUser);
   await saveSignup(mockSignup);
-  const insertedUser = await UserModel.findOne({
-    username: mockUser.username,
-  });
+  const insertedUser = await findUser(mockUser.username);
   expect(insertedUser?.signedGames.length).toEqual(2);
 
   await removeOverlapSignups(mockResults);
-  const updatedUser = await UserModel.findOne({
-    username: mockUser.username,
-  });
+  const updatedUser = await findUser(mockUser.username);
+
   expect(updatedUser?.signedGames.length).toEqual(1);
 });
