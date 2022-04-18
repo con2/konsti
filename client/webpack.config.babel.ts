a36bc0a8ae41e8301668cd7a07809e7f9346b661
Arttu Hanska <arttu.hanska@gmail.com>
2022-04-18 21:04:20 +0300

    Change Webpack sourcemap to eval-source-map (#310)

--- client/webpack.config.babel.ts
+++ client/webpack.config.babel.ts
@@ -90,7 +90,7 @@ const devConfig: Configuration = {
 
   mode: "development",
 
-  devtool: config.enableReduxTrace ? "source-map" : "eval", // Use eval for best hot-loading perf
+  devtool: config.enableReduxTrace ? "source-map" : "eval-source-map",
 
   // webpack-dev-server config
   // @ts-expect-error: ?
