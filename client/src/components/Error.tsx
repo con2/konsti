fd05e188c81ccf15a44a0fbdfd93b6baace8dbe6
Arttu Hanska <arttu.hanska@gmail.com>
2022-04-23 17:44:23 +0300

    Remove unused Error component (#317)

--- client/src/components/Error.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactElement } from "react";
-
-interface Props {
-  meta: { touched: boolean; error: string };
-}
-
-export const Error = ({ meta: { touched, error } }: Props): ReactElement => {
-  return touched && error ? <span>{error}</span> : <span />;
-};
--- client/src/components/Errors.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { Error } from "client/components/Error";
-
-const meta = { touched: true, error: "error" };
-
-test("should render correctly", () => {
-  render(<Error meta={meta} />);
-});
