31f8643ac3127448372c0ddd282f45432d5b736b
Arttu Hanska <arttu.hanska@gmail.com>
2022-04-30 22:15:10 +0300

    Fix incorrect login in removeSignedGame (#330)
    * Fix incorrect login in removeSignedGame
    
    * Toggle signup form closed after cancelin signup

--- client/src/views/all-games/components/allGamesUtils.ts
+++ client/src/views/all-games/components/allGamesUtils.ts
@@ -1,12 +1,11 @@
 import { Game } from "shared/typings/models/game";
-import { getUpcomingEnteredGames } from "client/utils/getUpcomingGames";
 import { SelectedGame } from "shared/typings/models/user";
 
 export const isAlreadySigned = (
   gameToCheck: Game,
   signedGames: readonly SelectedGame[]
 ): boolean => {
-  return [...signedGames].some(
+  return signedGames.some(
     (g: SelectedGame) => g.gameDetails.gameId === gameToCheck.gameId
   );
 };
@@ -15,9 +14,7 @@ export const isAlreadyEntered = (
   gameToCheck: Game,
   enteredGames: readonly SelectedGame[]
 ): boolean => {
-  const allEnteredGames = getUpcomingEnteredGames(enteredGames);
-
-  return [...allEnteredGames].some(
+  return enteredGames.some(
     (g: SelectedGame) => g.gameDetails.gameId === gameToCheck.gameId
   );
 };
