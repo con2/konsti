3771f1c444f84ea05f167bd11fa342c8bdc3cb1f
Arttu Hanska <arttu.hanska@gmail.com>
2022-05-06 21:15:50 +0300

    Update dependencies

--- client/src/views/all-games/AllGamesView.test.tsx
+++ client/src/views/all-games/AllGamesView.test.tsx
@@ -7,6 +7,23 @@ import * as loadData from "client/utils/loadData";
 
 jest.spyOn(loadData, "loadGames").mockReturnValue(Promise.resolve());
 
+jest.mock("@react-hook/intersection-observer", () => {
+  return {
+    __esModule: true,
+    default: () => {
+      return {
+        time: null,
+        rootBounds: null,
+        boundingClientRect: null,
+        intersectionRect: null,
+        intersectionRatio: null,
+        target: null,
+        isIntersecting: false,
+      };
+    },
+  };
+});
+
 test("should render correctly", async () => {
   await act(async () => {
     await render(
--- client/src/views/all-games/components/AllGamesList.test.tsx
+++ client/src/views/all-games/components/AllGamesList.test.tsx
@@ -9,6 +9,23 @@ import {
   Props,
 } from "client/views/all-games/components/AllGamesList";
 
+jest.mock("@react-hook/intersection-observer", () => {
+  return {
+    __esModule: true,
+    default: () => {
+      return {
+        time: null,
+        rootBounds: null,
+        boundingClientRect: null,
+        intersectionRect: null,
+        intersectionRatio: null,
+        target: null,
+        isIntersecting: false,
+      };
+    },
+  };
+});
+
 test("should render correctly", () => {
   const props: Props = {
     games: [testGame],
