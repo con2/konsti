0bb8582924550d5b880c3ab8bbc31b83ec622429
Arttu Hanska <arttu.hanska@gmail.com>
2022-05-08 21:57:38 +0300

    Fix local playwright server url

--- client/playwright/tests/utils.ts
+++ client/playwright/tests/utils.ts
@@ -1,7 +1,9 @@
 import { expect, APIRequestContext } from "@playwright/test";
 
 export const populateDb = async (request: APIRequestContext): Promise<void> => {
-  const url = `${process.env.PLAYWRIGHT_BASEURL}/api/populate-db`;
+  const url = `${
+    process.env.PLAYWRIGHT_BASEURL ?? `http://localhost:5000`
+  }/api/populate-db`;
   const response = await request.post(url);
   expect(response.status()).toBe(200);
 };
