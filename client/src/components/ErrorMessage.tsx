9ab081ac440ad8b311ad4b61be4ed3a68f31becc
Niko Kauppila <niko.kauppila@iki.fi>
2022-05-30 21:40:34 +0300

    Add some styles to error close button

--- client/src/components/ErrorMessage.tsx
+++ client/src/components/ErrorMessage.tsx
@@ -13,20 +13,32 @@ export const ErrorMessage = ({
 }: Props): ReactElement | null => {
   if (!message) return null;
   return (
-    <StyledErrorMessage>
-      {message} <StyledIcon onClick={closeError} icon="xmark" />
-    </StyledErrorMessage>
+    <ErrorContainer>
+      <StyledErrorMessage>{message}</StyledErrorMessage>
+      <StyledIcon onClick={closeError} icon="xmark" />
+    </ErrorContainer>
   );
 };
 
+const ErrorContainer = styled.div`
+  height: 24px;
+`;
+
 const StyledErrorMessage = styled.span`
+  line-height: 24px;
+  margin-right: 8px;
   font-weight: 600;
   color: ${(props) => props.theme.textError};
 `;
 
 const StyledIcon = styled(FontAwesomeIcon)`
+  border: 1px solid ${(props) => props.theme.errorCloseButtonBorder};
+  border-radius: 12px;
+  width: 18px;
+  height: 18px;
   cursor: pointer;
   font-size: ${(props) => props.theme.fontSizeLarge};
   vertical-align: middle;
-  margin-bottom: 1px;
+  margin-bottom: 2px;
+  background-color: ${(props) => props.theme.errorCloseButtonBackground};
 `;
