c89edf225e8c6ffbd698a0fff87e0ad1b1ac8d80
Arttu Hanska <arttu.hanska@gmail.com>
2022-06-08 22:35:50 +0300

    Add phase gap to frontend and backend (#370)
    * Add phase gap to frontend
    
    * Add phase gap backend check

--- client/src/test/test-components/testComponentUtils.ts
+++ client/src/test/test-components/testComponentUtils.ts
@@ -1,11 +1,17 @@
 import moment from "moment";
 import { sharedConfig } from "shared/config/sharedConfig";
 
-const { CONVENTION_START_TIME } = sharedConfig;
+const { CONVENTION_START_TIME, PHASE_GAP } = sharedConfig;
 
-export const times = [
+export const testTimes = [
   moment(CONVENTION_START_TIME).subtract(2, "hours").format(),
   moment(CONVENTION_START_TIME).format(),
+  moment(CONVENTION_START_TIME)
+    .add(PHASE_GAP - 2, "minutes")
+    .format(),
+  moment(CONVENTION_START_TIME)
+    .add(PHASE_GAP + 2, "minutes")
+    .format(),
   moment(CONVENTION_START_TIME).add(45, "minutes").format(),
   moment(CONVENTION_START_TIME).add(1, "hours").add(45, "minutes").format(),
   moment(CONVENTION_START_TIME).add(2, "hours").add(45, "minutes").format(),
