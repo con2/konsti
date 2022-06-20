ab3bf787425211b12c52a2d2a324e8e5673790e3
Arttu Hanska <arttu.hanska@gmail.com>
2022-06-20 20:50:49 +0300

    Fix test

--- server/src/features/user/signed-game/signedGameController.test.ts
+++ server/src/features/user/signed-game/signedGameController.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { startTestServer, stopTestServer } from "server/test/utils/testServer";
 import { ApiEndpoint } from "shared/constants/apiEndpoints";
+import { SignupData } from "shared/typings/api/myGames";
 
 jest.mock("server/utils/logger");
 
@@ -23,15 +24,17 @@ describe(`POST ${ApiEndpoint.SIGNED_GAME}`, () => {
   test("should return 401 without valid authorization", async () => {
     const { server, mongoServer } = await startTestServer();
 
+    const signupData: SignupData = {
+      username: "testuser",
+      selectedGames: [],
+      startTime: "2019-11-23T08:00:00Z",
+    };
+
     try {
       const response = await request(server)
         .post(ApiEndpoint.SIGNED_GAME)
         .send({
-          signupData: {
-            username: "testuser",
-            selectedGames: [],
-            signupTime: "2019-11-23T08:00:00Z",
-          },
+          signupData,
         });
       expect(response.status).toEqual(401);
     } finally {
