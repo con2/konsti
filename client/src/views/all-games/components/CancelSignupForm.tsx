ea62db82aeac2cc46352297fdbac156ddc976147
Arttu Hanska <arttu.hanska@gmail.com>
2022-06-21 21:49:28 +0300

    Add cancel to pre-signup confirm message

--- client/src/views/all-games/components/CancelSignupForm.tsx
+++ client/src/views/all-games/components/CancelSignupForm.tsx
@@ -1,76 +1,35 @@
-import React, { FC, ReactElement, FormEvent, useState } from "react";
+import React, { ReactElement } from "react";
 import styled from "styled-components";
 import { useTranslation } from "react-i18next";
-import { Game } from "shared/typings/models/game";
-import { useAppDispatch, useAppSelector } from "client/utils/hooks";
 import { Button, ButtonStyle } from "client/components/Button";
-import {
-  DeleteEnteredGameErrorMessage,
-  submitDeleteEnteredGame,
-} from "client/views/my-games/myGamesThunks";
-import { loadGames } from "client/utils/loadData";
-import { ErrorMessage } from "client/components/ErrorMessage";
 
 interface Props {
-  game: Game;
-  onCancelSignup: () => void;
   onCancelForm: () => void;
+  onConfirmForm: () => Promise<void>;
 }
 
-export const CancelSignupForm: FC<Props> = (props: Props): ReactElement => {
-  const { game, onCancelSignup, onCancelForm } = props;
-
+export const CancelSignupForm = ({
+  onCancelForm,
+  onConfirmForm,
+}: Props): ReactElement => {
   const { t } = useTranslation();
-  const dispatch = useAppDispatch();
-  const username = useAppSelector((state) => state.login.username);
-
-  const [serverError, setServerError] =
-    useState<DeleteEnteredGameErrorMessage | null>(null);
-
-  const handleCancel = (): void => {
-    onCancelForm();
-  };
-  const removeSignup = async (event: FormEvent): Promise<void> => {
-    const errorMessage = await dispatch(
-      submitDeleteEnteredGame({
-        username,
-        startTime: game.startTime,
-        enteredGameId: game.gameId,
-      })
-    );
-
-    if (errorMessage) {
-      setServerError(errorMessage);
-    } else {
-      await loadGames();
-      onCancelSignup();
-    }
-  };
 
   return (
-    <>
-      <ButtonContainer>
-        <CancelSignupButton
-          onClick={removeSignup}
-          buttonStyle={ButtonStyle.WARNING}
-        >
-          {t("signup.confirmCancelSignup")}
-        </CancelSignupButton>
-        <CancelFormButton
-          onClick={handleCancel}
-          buttonStyle={ButtonStyle.NORMAL}
-        >
-          {t("signup.cancel")}
-        </CancelFormButton>
-      </ButtonContainer>
+    <ButtonContainer>
+      <CancelSignupButton
+        onClick={onConfirmForm}
+        buttonStyle={ButtonStyle.WARNING}
+      >
+        {t("signup.confirmCancelSignup")}
+      </CancelSignupButton>
 
-      {serverError && (
-        <ErrorMessage
-          message={t(serverError)}
-          closeError={() => setServerError(null)}
-        />
-      )}
-    </>
+      <CancelFormButton
+        onClick={() => onCancelForm()}
+        buttonStyle={ButtonStyle.NORMAL}
+      >
+        {t("signup.cancel")}
+      </CancelFormButton>
+    </ButtonContainer>
   );
 };
 
--- client/src/views/my-games/components/ResultsByStartTimes.tsx
+++ client/src/views/my-games/components/ResultsByStartTimes.tsx
@@ -1,13 +1,9 @@
-import React, { ReactElement, useState } from "react";
-import { Link } from "react-router-dom";
-import { useTranslation } from "react-i18next";
+import React, { ReactElement } from "react";
 import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useTranslation } from "react-i18next";
 import { timeFormatter } from "client/utils/timeFormatter";
-import { useAppSelector } from "client/utils/hooks";
 import { SelectedGame } from "shared/typings/models/user";
-import { CancelSignupForm } from "client/views/all-games/components/CancelSignupForm";
-import { Button, ButtonStyle } from "client/components/Button";
+import { EnteredGameRow } from "client/views/my-games/components/EnteredGameRow";
 
 interface Props {
   signups: readonly SelectedGame[];
@@ -20,12 +16,8 @@ export const ResultsByStartTimes = ({
   startTimes,
   missedSignups,
 }: Props): ReactElement => {
-  const [showCancelSignupForm, setShowCancelSignupForm] = useState<String[]>(
-    []
-  );
-
-  const signupMessages = useAppSelector((state) => state.admin.signupMessages);
   const { t } = useTranslation();
+
   return (
     <div>
       {startTimes.map((startTime) => {
@@ -37,77 +29,22 @@ export const ResultsByStartTimes = ({
                 capitalize: true,
               })}
             </StyledTime>
+
             {signups.map((signup) => {
-              const cancelSignupFormVisible = showCancelSignupForm.find(
-                (signupform) => signupform === signup.gameDetails.gameId
+              return (
+                <EnteredGameRow
+                  key={signup.gameDetails.gameId}
+                  signup={signup}
+                  startTime={startTime}
+                />
               );
-              const onCancelForm = (): void => {
-                setShowCancelSignupForm(
-                  showCancelSignupForm.filter(
-                    (signupform) => signupform !== signup.gameDetails.gameId
-                  )
-                );
-              };
-
-              const onCancelSignup = (): void => {
-                setShowCancelSignupForm(
-                  showCancelSignupForm.filter(
-                    (signupform) => signupform !== signup.gameDetails.gameId
-                  )
-                );
-              };
-              const onConfirmCancelSignup = (): void => {
-                setShowCancelSignupForm([
-                  ...showCancelSignupForm,
-                  signup.gameDetails.gameId,
-                ]);
-              };
-
-              if (signup.time === startTime) {
-                const showSignupMessage = signupMessages.find(
-                  (message) => message.gameId === signup.gameDetails.gameId
-                );
-                return (
-                  <GameDetailsList key={signup.gameDetails.gameId}>
-                    <Link to={`/games/${signup.gameDetails.gameId}`}>
-                      {signup.gameDetails.title}
-                    </Link>
-
-                    <ButtonContainer>
-                      {cancelSignupFormVisible ? (
-                        <CancelSignupForm
-                          game={signup.gameDetails}
-                          onCancelForm={onCancelForm}
-                          onCancelSignup={onCancelSignup}
-                        />
-                      ) : (
-                        <Button
-                          onClick={() => onConfirmCancelSignup()}
-                          buttonStyle={ButtonStyle.NORMAL}
-                        >
-                          {t("button.cancelSignup")}
-                        </Button>
-                      )}
-                    </ButtonContainer>
-
-                    {!!showSignupMessage && (
-                      <SignupMessagePlacement>
-                        <FontAwesomeIcon icon={"comment"} />
-                        {` ${t("myGamesView.yourAnswer")} "${
-                          showSignupMessage.message
-                        }": ${signup.message}`}
-                      </SignupMessagePlacement>
-                    )}
-                  </GameDetailsList>
-                );
-              }
             })}
 
             {missedSignups.map((missedSignup) => {
               if (missedSignup === startTime) {
                 return (
                   <GameDetailsList key={missedSignup}>
-                    {t("noSignupResult")}
+                    {t("noPreSignupResult")}
                   </GameDetailsList>
                 );
               }
@@ -123,14 +60,6 @@ const GameDetailsList = styled.div`
   display: flex;
   align-items: center;
   padding-left: 30px;
-`;
-
-const ButtonContainer = styled.div`
-  padding-left: 10px;
-`;
-
-const SignupMessagePlacement = styled.div`
-  padding-top: 5px;
 `;
 
 const StyledTime = styled.p`
