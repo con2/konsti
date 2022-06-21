8d94f20aaf47083a95d1483b5d8d1861f2424461
Arttu Hanska <arttu.hanska@gmail.com>
2022-06-21 21:57:02 +0300

    Change MyGames signup text to pre-signup

--- client/src/views/my-games/components/MySignupsList.tsx
+++ client/src/views/my-games/components/MySignupsList.tsx
@@ -28,7 +28,7 @@ export const MySignupsList = ({
 
   return (
     <div>
-      <h3>{t("signedGames")}</h3>
+      <h3>{t("preSignedGames")}</h3>
 
       {!isGroupCreator && <InfoText>{t("group.inGroupSignups")}</InfoText>}
 
