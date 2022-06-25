60558a3708fa1245bf811a5e34c5d96e9f9cedd4
Arttu Hanska <arttu.hanska@gmail.com>
2022-06-25 22:05:34 +0300

    Change selection to dropdown

--- client/src/components/EventTypeSelection.tsx
+++ client/src/components/EventTypeSelection.tsx
@@ -1,9 +1,10 @@
-import React, { ReactElement } from "react";
+import React, { ChangeEvent, ReactElement } from "react";
 import { useTranslation } from "react-i18next";
+import styled from "styled-components";
 import { useAppDispatch, useAppSelector } from "client/utils/hooks";
 import { ProgramType } from "shared/typings/models/game";
-import { Button, ButtonStyle } from "client/components/Button";
 import { setActiveProgramType } from "client/views/admin/adminSlice";
+import { Dropdown } from "client/components/Dropdown";
 
 export const ProgramTypeSelection = (): ReactElement => {
   const { t } = useTranslation();
@@ -15,23 +16,26 @@ export const ProgramTypeSelection = (): ReactElement => {
 
   const programTypes = Object.values(ProgramType);
 
+  const dropdownItems = programTypes.map((programType) => ({
+    value: programType,
+    title: t(`programType.${programType}`),
+  }));
+
   return (
     <>
-      {programTypes.map((programType) => {
-        return (
-          <Button
-            key={programType}
-            onClick={() => dispatch(setActiveProgramType(programType))}
-            buttonStyle={
-              activeProgramType === programType
-                ? ButtonStyle.DISABLED
-                : ButtonStyle.NORMAL
-            }
-          >
-            {t(`programType.${programType}`)}
-          </Button>
-        );
-      })}
+      <InfoText>{t("selectedProgramType")}</InfoText>
+      <Dropdown
+        items={dropdownItems}
+        selectedValue={activeProgramType}
+        onChange={(event: ChangeEvent<HTMLSelectElement>) =>
+          dispatch(setActiveProgramType(event.target.value as ProgramType))
+        }
+        loading={false}
+      />
     </>
   );
 };
+
+const InfoText = styled.span`
+  margin-right: 8px;
+`;
