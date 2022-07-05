4dd6c12041d39a771cd55c65ebee8a39bfa3e391
Arttu Hanska <arttu.hanska@gmail.com>
2022-07-05 13:56:06 +0300

    Remove Discord guides and term 'game' from couple of places (#423)

--- client/src/views/registration/RegistrationView.tsx
+++ client/src/views/registration/RegistrationView.tsx
@@ -1,29 +1,6 @@
 import React, { ReactElement } from "react";
-import { useTranslation } from "react-i18next";
-import styled from "styled-components";
 import { RegistrationForm } from "client/views/registration/components/RegistrationForm";
 
 export const RegistrationView = (): ReactElement => {
-  const { t } = useTranslation();
-
-  return (
-    <div>
-      <NotificationMessage>
-        {t("pageTitle.usernamePublicWarning")}.{" "}
-        {t("pageTitle.usernameDiscordInfo")}
-        <p>
-          <a href={"https://2021.ropecon.fi/etaropecon/ohjeet/"}>
-            {t("discordHint")}
-          </a>
-        </p>
-      </NotificationMessage>
-
-      <RegistrationForm />
-    </div>
-  );
+  return <RegistrationForm />;
 };
-
-const NotificationMessage = styled.div`
-  color: ${(props) => props.theme.textWarning};
-  margin: 20px 0 0 0;
-`;
