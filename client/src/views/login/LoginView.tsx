70203e086a928e945d0ca2dd3d2270b2030733d7
Arttu Hanska <arttu.hanska@gmail.com>
2022-07-09 21:43:50 +0300

    Add registration link to login page (#437)

--- client/src/views/login/LoginView.tsx
+++ client/src/views/login/LoginView.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from "react";
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 import { LoginForm } from "client/views/login/components/LoginForm";
 
 export const LoginView = (): ReactElement => {
@@ -8,7 +9,12 @@ export const LoginView = (): ReactElement => {
   return (
     <div>
       <h2>{t("pageTitle.login")}</h2>
+      <p>{t("loginView.oldAccountsNotWorking")}</p>
       <LoginForm />
+
+      <Link to={`/registration`}>
+        <p>{t("loginView.noAccountRegister")}</p>
+      </Link>
     </div>
   );
 };
