75c49d2831d11080a2adbd16915a79ab7bd7e626
Arttu Hanska <arttu.hanska@gmail.com>
2022-07-15 17:18:30 +0300

    Add more test times and hide strategy test value

--- client/src/test/test-components/SignupStrategySelector.tsx
+++ client/src/test/test-components/SignupStrategySelector.tsx
@@ -4,8 +4,9 @@ import { SignupStrategy } from "shared/config/sharedConfig.types";
 import { Dropdown } from "client/components/Dropdown";
 import { useAppDispatch, useAppSelector } from "client/utils/hooks";
 import { submitSetSignupStrategy } from "client/views/admin/adminThunks";
+import { config } from "client/config";
 
-export const SignupStrategySelector = (): ReactElement => {
+export const SignupStrategySelector = (): ReactElement | null => {
   const signupStrategy = useAppSelector((state) => state.admin.signupStrategy);
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
@@ -19,6 +20,10 @@ export const SignupStrategySelector = (): ReactElement => {
       title: t("strategies.algorithm+direct"),
     },
   ];
+
+  if (!config.enableStrategyTestValue) {
+    return null;
+  }
 
   return (
     <div>
