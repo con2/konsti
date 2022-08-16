b3383257b4e876eda843eba8af8228deeafafb49
Arttu Hanska <arttu.hanska@gmail.com>
2022-08-16 16:06:07 +0300

    Add Ropecon 2022 data (#519)

--- server/src/features/statistics/fixer-helpers/gameIdFix.ts
+++ server/src/features/statistics/fixer-helpers/gameIdFix.ts
@@ -7,6 +7,8 @@ import { ResultsCollectionEntry } from "server/typings/result.typings";
 import { writeJson } from "server/features/statistics/statsUtil";
 import { config } from "server/config";
 import { Game } from "shared/typings/models/game";
+import { SignupDoc } from "server/features/signup/signup.typings";
+import { SettingsDoc } from "server/typings/settings.typings";
 
 export const gameIdFix = (year: number, event: string): void => {
   const users: User[] = JSON.parse(
@@ -36,9 +38,26 @@ export const gameIdFix = (year: number, event: string): void => {
 
   logger.info(`Loaded ${games.length} games`);
 
+  const signups: SignupDoc[] = JSON.parse(
+    fs.readFileSync(
+      `${config.statsDataDir}/${event}/${year}/signups.json`,
+      "utf8"
+    )
+  );
+
+  logger.info(`Loaded ${games.length} games`);
+
+  const settings: SettingsDoc[] = JSON.parse(
+    fs.readFileSync(
+      `${config.statsDataDir}/${event}/${year}/settings.json`,
+      "utf8"
+    )
+  );
+
+  logger.info(`Loaded ${settings.length} games`);
+
   users.forEach((user) => {
     const tempFavoritedGames: Game[] = [];
-    // const tempEnteredGames: SelectedGame[] = [];
     const tempSignedGames: SelectedGame[] = [];
 
     games.forEach((game) => {
@@ -48,19 +67,6 @@ export const gameIdFix = (year: number, event: string): void => {
           tempFavoritedGames.push({ gameId: game.gameId });
         }
       });
-
-      // TODO: Update to use signup collection
-      /*
-      user.enteredGames.forEach((enteredGame) => {
-        if (_.isEqual(game._id, enteredGame.gameDetails)) {
-          tempEnteredGames.push({
-            ...enteredGame,
-            // @ts-expect-error: We don't want whole game details
-            gameDetails: { gameId: game.gameId },
-          });
-        }
-      });
-      */
 
       user.signedGames.forEach((signedGame) => {
         if (_.isEqual(game._id, signedGame.gameDetails)) {
@@ -74,7 +80,6 @@ export const gameIdFix = (year: number, event: string): void => {
     });
 
     user.favoritedGames = tempFavoritedGames;
-    // user.enteredGames = tempEnteredGames;
     user.signedGames = tempSignedGames;
   });
 
@@ -84,13 +89,40 @@ export const gameIdFix = (year: number, event: string): void => {
         if (_.isEqual(game._id, userResult.enteredGame.gameDetails)) {
           userResult.enteredGame = {
             ...userResult.enteredGame,
-            gameDetails: game,
+            // @ts-expect-error: We don't want whole game details
+            gameDetails: { gameId: game.gameId },
           };
         }
       });
     });
   });
 
+  signups.forEach((signup) => {
+    games.forEach((game) => {
+      if (_.isEqual(game._id, signup.game)) {
+        // @ts-expect-error: We don't want whole game details
+        signup.game = { gameId: game.gameId };
+      }
+    });
+  });
+
+  const tempHiddenGames: Game[] = [];
+
+  settings.forEach((setting) => {
+    games.forEach((game) => {
+      setting.hiddenGames.forEach((hiddenGame) => {
+        if (_.isEqual(game._id, hiddenGame)) {
+          // @ts-expect-error: We don't want whole game details
+          tempHiddenGames.push({ gameId: game.gameId });
+        }
+      });
+    });
+  });
+
+  settings[0].hiddenGames = tempHiddenGames;
+
   writeJson(year, event, "users", users);
   writeJson(year, event, "results", results);
+  writeJson(year, event, "signups", signups);
+  writeJson(year, event, "settings", settings);
 };
