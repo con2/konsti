e870afc4f066fdf5341a6fead0e3f7f5cba636e3
Maija Nevala <>
2022-10-25 10:12:54 +0300

    Code review fixes.

--- client/src/components/TextArea.tsx
+++ client/src/components/TextArea.tsx
@@ -1,6 +1,32 @@
+import React, { ChangeEvent, ReactElement } from "react";
 import styled from "styled-components";
 
-export const Textarea = styled.textarea`
+interface Props {
+  className?: string;
+  onChange: (event: ChangeEvent<HTMLTextAreaElement>) => void;
+  rows?: number;
+  value: string;
+}
+
+const DEFAULT_ROW_COUNT = 4;
+
+export const TextArea = ({
+  className,
+  onChange,
+  rows = DEFAULT_ROW_COUNT,
+  value,
+}: Props): ReactElement => {
+  return (
+    <StyledTextArea
+      className={className}
+      onChange={onChange}
+      rows={rows}
+      value={value}
+    />
+  );
+};
+
+const StyledTextArea = styled.textarea`
   border: 1px solid ${(props) => props.theme.borderInactive};
   resize: none;
   overflow: auto;
