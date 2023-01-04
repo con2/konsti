c7a1fa5d5086fbd8adf74e158f54bffe6ef17822
Arttu Hanska <arttu.hanska@gmail.com>
2023-01-04 21:32:24 +0200

    Update other TS-related dependencies

--- server/src/utils/server.ts
+++ server/src/utils/server.ts
@@ -98,6 +98,7 @@ export const startServer = async ({
       limit: "5000kb", // limit: 5MB
       type: "text/plain",
     }),
+    /* eslint-disable-next-line @typescript-eslint/no-misused-promises */
     async (req, res) => {
       await postSentryTunnel(req, res);
     }
