c4acf806d1e00ec7a5ebad2fd59306a33777e143
Arttu Hanska <arttu.hanska@gmail.com>
2023-02-05 20:47:12 +0200

    Improve Sentry testing and move Sentry stuff to own file

--- server/src/features/sentry-tunnel/sentryTunnelController.ts
+++ server/src/features/sentry-tunnel/sentryTunnelController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { logger } from "server/utils/logger";
 import { ApiEndpoint } from "shared/constants/apiEndpoints";
 import { resendSentryRequest } from "server/features/sentry-tunnel/sentryTunnelService";
+import { UserGroup } from "shared/typings/models/user";
+import { isAuthorized } from "server/utils/authHeader";
 
 export const postSentryTunnel = async (
   req: Request<{}, {}, null>,
@@ -12,4 +14,17 @@ export const postSentryTunnel = async (
   if (!req.body) return res.sendStatus(422);
   const response = await resendSentryRequest(req.body);
   return res.json(response);
+};
+
+export const getSentryTest = (
+  req: Request<{}, {}, null>,
+  res: Response
+): Response => {
+  logger.info(`API call: POST ${ApiEndpoint.SENTRY_TEST}`);
+
+  if (!isAuthorized(req.headers.authorization, UserGroup.ADMIN, "admin")) {
+    return res.sendStatus(401);
+  }
+
+  throw new Error("Test Sentry error");
 };
---
+++ server/src/utils/sentry.ts
@@ -0,0 +1,65 @@
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+import express, { Express } from "express";
+import helmet from "helmet";
+import { postSentryTunnel } from "server/features/sentry-tunnel/sentryTunnelController";
+import { sharedConfig } from "shared/config/sharedConfig";
+import { ApiEndpoint } from "shared/constants/apiEndpoints";
+
+export const initSentry = (app: Express, enableSentry: boolean): void => {
+  Sentry.init({
+    dsn: getDsn(enableSentry),
+    integrations: [
+      // Enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // Enable Express.js middleware tracing
+      new Tracing.Integrations.Express({
+        // To trace all requests to the default router
+        app,
+      }),
+    ],
+    tracesSampleRate: sharedConfig.tracesSampleRate,
+    environment: process.env.SETTINGS,
+  });
+
+  // The request handler must be the first middleware on the app
+  // RequestHandler creates a separate execution context using domains, so that every
+  // transaction/span/breadcrumb is attached to its own Hub instance
+  app.use(Sentry.Handlers.requestHandler());
+
+  // TracingHandler creates a trace for every incoming request
+  app.use(Sentry.Handlers.tracingHandler());
+
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        "connect-src": ["'self'", "*.sentry.io"],
+      },
+    })
+  );
+
+  // Sentry tunnel endpoint which accepts text/plain payload
+  app.post(
+    ApiEndpoint.SENTRY_TUNNEL,
+    express.text({
+      limit: "5000kb", // limit: 5MB
+      type: "text/plain",
+    }),
+    /* eslint-disable-next-line @typescript-eslint/no-misused-promises */
+    async (req, res) => {
+      await postSentryTunnel(req, res);
+    }
+  );
+};
+
+const getDsn = (enableSentry: boolean): string | undefined => {
+  if (!enableSentry) return undefined;
+  switch (process.env.SETTINGS) {
+    case "production":
+      return "https://0278d6bfb3f04c70acf826ecbd86ae58@o1321706.ingest.sentry.io/6579204";
+    case "staging":
+      return "https://ab176c60aac24be8af2f6c790f1437ac@o1321706.ingest.sentry.io/6578390";
+    default:
+      return undefined;
+  }
+};
