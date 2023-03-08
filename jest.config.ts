4397e2d7decf257fd9cfad0fdff27bb0cc2523e8
Arttu Hanska <arttu.hanska@gmail.com>
2023-03-08 19:17:16 +0200

    Set Jest config workerIdleMemoryLimit

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { JestConfigWithTsJest } from "ts-jest";
 const jestConfig: JestConfigWithTsJest = {
   projects: ["client", "server"],
   coverageDirectory: "coverage",
+  workerIdleMemoryLimit: "1.5GB", // https://github.com/facebook/jest/issues/11956
 };
 
 /* eslint-disable-next-line import/no-unused-modules */
