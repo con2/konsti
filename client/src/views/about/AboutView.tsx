8d553ab3dfe1482cf5194c3b8c07d7f5bf716575
Maija Nevala <>
2023-04-24 19:23:13 +0300

    Divide about page into two components.

--- client/src/views/about/AboutView.tsx
+++ client/src/views/about/AboutView.tsx
@@ -1,65 +1,16 @@
-import React, { ReactElement, useState } from "react";
+import React, { ReactElement } from "react";
 import { useTranslation } from "react-i18next";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import { Button, ButtonStyle } from "client/components/Button";
 import aboutKonstiFi from "client/markdown/AboutKonstiFi.md";
 import aboutKonstiEn from "client/markdown/AboutKonstiEn.md";
-import konstiFaqFi from "client/markdown/KonstiFaqFi.md";
-import konstiFaqEn from "client/markdown/KonstiFaqEn.md";
-import { ButtonGroup } from "client/components/ButtonGroup";
-import { Tabs } from "client/components/Tabs";
-
-enum AboutSection {
-  FAQ = "faq",
-  GENERAL = "general",
-}
 
 export const AboutView = (): ReactElement => {
-  const { t, i18n } = useTranslation();
-
-  const [selectedSection, setSelectedSection] = useState<AboutSection>(
-    AboutSection.GENERAL
-  );
-
-  /*
-        <Tabs
-        tabContents={[
-          { headerText: "test", path: "foo", content: Foo },
-          { headerText: "test2", path: "bar", content: Bar },
-        ]}
-      />
-   */
+  const { i18n } = useTranslation();
 
   return (
-    <div>
-      <ButtonGroup>
-        <Button
-          disabled={selectedSection === AboutSection.GENERAL}
-          buttonStyle={ButtonStyle.SECONDARY}
-          onClick={() => setSelectedSection(AboutSection.GENERAL)}
-        >
-          {t("aboutView.general")}
-        </Button>
-        <Button
-          disabled={selectedSection === AboutSection.FAQ}
-          buttonStyle={ButtonStyle.SECONDARY}
-          onClick={() => setSelectedSection(AboutSection.FAQ)}
-        >
-          {t("aboutView.faq")}
-        </Button>
-      </ButtonGroup>
-      {selectedSection === AboutSection.GENERAL && (
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>
-          {i18n.language === "fi" ? aboutKonstiFi : aboutKonstiEn}
-        </ReactMarkdown>
-      )}
-
-      {selectedSection === AboutSection.FAQ && (
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>
-          {i18n.language === "fi" ? konstiFaqFi : konstiFaqEn}
-        </ReactMarkdown>
-      )}
-    </div>
+    <ReactMarkdown remarkPlugins={[remarkGfm]}>
+      {i18n.language === "fi" ? aboutKonstiFi : aboutKonstiEn}
+    </ReactMarkdown>
   );
 };
--- client/src/views/about/FaqView.tsx
+++ client/src/views/about/FaqView.tsx
@@ -1,5 +1,16 @@
 import React, { ReactElement } from "react";
+import { useTranslation } from "react-i18next";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import konstiFaqFi from "client/markdown/KonstiFaqFi.md";
+import konstiFaqEn from "client/markdown/KonstiFaqEn.md";
 
 export const FaqView = (): ReactElement => {
-  return <div>THIS IS THE FAQ</div>;
+  const { i18n } = useTranslation();
+
+  return (
+    <ReactMarkdown remarkPlugins={[remarkGfm]}>
+      {i18n.language === "fi" ? konstiFaqFi : konstiFaqEn}
+    </ReactMarkdown>
+  );
 };
