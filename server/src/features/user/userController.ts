7a2735f080b15aaa9089d29a2d9119bfd30fb179
Arttu Hanska <arttu.hanska@gmail.com>
2023-05-08 21:32:43 +0300

    Fix conditional validation logic

--- server/src/features/user/userController.ts
+++ server/src/features/user/userController.ts
@@ -43,7 +43,7 @@ export const postUser = async (
       .optional()
       .refine((input) => {
         if (sharedConfig.requireRegistrationCode) {
-          if (input?.trim().length === 0) {
+          if (!input || input.trim().length === 0) {
             return false;
           }
         }
