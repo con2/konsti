37b4e224606126b0f07630643bd6468e28cf1126
Arttu Hanska <arttu.hanska@gmail.com>
2023-05-08 21:40:20 +0300

    Improve ordering

--- shared/typings/api/users.ts
+++ shared/typings/api/users.ts
@@ -2,18 +2,18 @@ import { ApiError } from "shared/typings/api/errors";
 import { Game } from "shared/typings/models/game";
 import { SelectedGame } from "shared/typings/models/user";
 
-export interface PostUserRequest {
-  username: string;
-  password: string;
-  serial?: string;
-}
-
 export interface GetUserResponse {
   games: UserGames;
   message: string;
   serial: string;
   status: "success";
   username: string;
+}
+
+export interface PostUserRequest {
+  username: string;
+  password: string;
+  serial?: string;
 }
 
 export interface PostUserResponse {
