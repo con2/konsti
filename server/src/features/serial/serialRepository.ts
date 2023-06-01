6de13bfaf58e3241931439811923040790a9acd2
Arttu Hanska <arttu.hanska@gmail.com>
2023-06-01 19:48:19 +0300

    Fix regression

--- server/src/features/serial/serialRepository.ts
+++ server/src/features/serial/serialRepository.ts
@@ -4,8 +4,10 @@ import { SerialModel } from "server/features/serial/serialSchema";
 import { SerialDoc, Serial } from "server/typings/serial.typings";
 import {
   AsyncResult,
+  isErrorResult,
   makeErrorResult,
   makeSuccessResult,
+  unwrapResult,
 } from "shared/utils/asyncResult";
 import { MongoDbError } from "shared/typings/api/errors";
 
@@ -15,13 +17,17 @@ export const saveSerials = async (
   const serialDocs = [] as SerialDoc[];
   // create serials
   for (let i = 1; i <= count; i += 1) {
-    const serial: string = generator.generate(10);
+    const serial = generator.generate(10);
     const rawSerials = serialDocs.map((serialDoc) => serialDoc.serial);
 
-    if (
-      (await findSerial(serial)) ||
-      rawSerials.filter((s) => s === serial).length > 0
-    ) {
+    const findSerialAsyncResult = await findSerial(serial);
+    if (isErrorResult(findSerialAsyncResult)) {
+      return makeErrorResult(MongoDbError.UNKNOWN_ERROR);
+    }
+
+    const existingSerial = unwrapResult(findSerialAsyncResult);
+
+    if (existingSerial || rawSerials.filter((s) => s === serial).length > 0) {
       i -= 1;
       continue;
     }
