96af10065f4aa9c9ea5969c96a9c1f0bc0a4f0f2
Arttu Hanska <arttu.hanska@gmail.com>
2023-06-19 21:49:29 +0300

    Toggle events seen on unmount and page reload

--- client/src/views/all-games/components/EventLog.tsx
+++ client/src/views/all-games/components/EventLog.tsx
@@ -1,15 +1,44 @@
-import { ReactElement } from "react";
+import { ReactElement, useCallback, useEffect } from "react";
 import dayjs from "dayjs";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { useAppSelector } from "client/utils/hooks";
+import { useAppDispatch, useAppSelector } from "client/utils/hooks";
+import { submitUpdateEventLogIsSeen } from "client/views/login/loginThunks";
+import { FavoriteButton } from "client/components/FavoriteButton";
 
 export const EventLog = (): ReactElement => {
   const { t } = useTranslation();
+  const dispatch = useAppDispatch();
 
   const eventLogItems = useAppSelector((state) => state.login.eventLogItems);
   const games = useAppSelector((state) => state.allGames.games);
+  const username = useAppSelector((state) => state.login.username);
+
+  window.onbeforeunload = () => {
+    setEventsSeen();
+  };
+
+  const setEventsSeen = useCallback((): void => {
+    eventLogItems.map(({ eventLogItemId, isSeen }) => {
+      if (isSeen) {
+        return;
+      }
+      dispatch(
+        submitUpdateEventLogIsSeen({
+          username,
+          eventLogItemId,
+          isSeen: true,
+        })
+      );
+    });
+  }, [dispatch, eventLogItems, username]);
+
+  useEffect(() => {
+    return () => {
+      setEventsSeen();
+    };
+  }, [setEventsSeen]);
 
   return (
     <div>
@@ -30,7 +59,6 @@ export const EventLog = (): ReactElement => {
                 ({t("eventLog.sentTimeAgo")}{" "}
                 {dayjs().to(eventLogItem.createdAt)})
               </EventTitle>
-              {/*
               <FavoriteButton
                 isFavorite={eventLogItem.isSeen}
                 onClick={async () => {
@@ -43,7 +71,6 @@ export const EventLog = (): ReactElement => {
                   );
                 }}
               />
-              */}
             </div>
           );
         })}
