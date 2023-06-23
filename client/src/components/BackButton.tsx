4376da2a129525241ffa7c10ca5b2fe0f92fc0a0
Maija Nevala <>
2023-06-23 15:46:53 +0300

    Add explanatory comment to back button logic.

--- client/src/components/BackButton.tsx
+++ client/src/components/BackButton.tsx
@@ -9,6 +9,7 @@ export const BackButton = (): ReactElement => {
   const { t } = useTranslation();
 
   const goBack = (): void => {
+    // Navigate to front page if no previous page exists
     window.history.state?.idx > 0 ? navigate(-1) : navigate("/");
   };
 
