40c5d1f7c8cccf7843b4ad3b75a9082e5ca25a16
Arttu Hanska <arttu.hanska@gmail.com>
2023-06-28 19:48:22 +0300

    Change create account to log in (#640)

--- client/src/views/registration/RegistrationView.tsx
+++ client/src/views/registration/RegistrationView.tsx
@@ -1,6 +1,17 @@
 import { ReactElement } from "react";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import { RegistrationForm } from "client/views/registration/components/RegistrationForm";
 
 export const RegistrationView = (): ReactElement => {
-  return <RegistrationForm />;
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <RegistrationForm />{" "}
+      <Link to={`/login`}>
+        <p>{t("registrationView.alreadyHaveAccountLogin")}</p>
+      </Link>
+    </>
+  );
 };
