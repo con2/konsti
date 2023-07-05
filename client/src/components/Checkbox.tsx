7ebe8e211999849e3d10235c8178e84005f598a5
Arttu Hanska <arttu.hanska@gmail.com>
2023-07-05 21:01:39 +0300

    Make checkbox label clickable (#655)

---
+++ client/src/components/Checkbox.tsx
@@ -0,0 +1,33 @@
+import { ReactElement } from "react";
+import styled from "styled-components";
+
+interface Props {
+  checked: boolean;
+  onChange: () => void;
+  label: string;
+  id: string;
+}
+
+export const Checkbox = ({
+  checked,
+  onChange,
+  label,
+  id,
+}: Props): ReactElement => {
+  return (
+    <div>
+      <StyledCheckbox
+        type={"checkbox"}
+        checked={checked}
+        onChange={onChange}
+        id={id}
+      />
+      <label htmlFor={id}>{label}</label>
+    </div>
+  );
+};
+
+const StyledCheckbox = styled.input`
+  margin-right: 10px;
+  width: 16px;
+`;
