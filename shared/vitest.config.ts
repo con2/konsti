da63c72aeab8bdf78271469b83c1b264bc01988c
Arttu Hanska <arttu.hanska@gmail.com>
2023-07-10 21:09:35 +0300

    Add setupTests

--- shared/vitest.config.ts
+++ shared/vitest.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
+    setupFiles: ["./setupTests.ts"],
   },
   resolve: {
     alias: {
