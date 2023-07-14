50a4cf2cc0a6a8ac3401d31da8cd6e872b892fcb
Arttu Hanska <arttu.hanska@gmail.com>
2023-07-14 13:38:30 +0300

    If other assignment fails, return other

--- server/src/features/player-assignment/utils/runAssignmentStrategy.ts
+++ server/src/features/player-assignment/utils/runAssignmentStrategy.ts
@@ -5,13 +5,17 @@ import { padgAssignPlayers } from "server/features/player-assignment/padg/padgAs
 import { randomAssignPlayers } from "server/features/player-assignment/random/randomAssignPlayers";
 import { User } from "shared/typings/models/user";
 import { Game } from "shared/typings/models/game";
-import { PlayerAssignmentResult } from "server/typings/result.typings";
+import {
+  AssignmentResultStatus,
+  PlayerAssignmentResult,
+} from "server/typings/result.typings";
 import { AssignmentStrategy } from "shared/config/sharedConfig.types";
 import { Signup } from "server/features/signup/signup.typings";
 import { exhaustiveSwitchGuard } from "shared/utils/exhaustiveSwitchGuard";
 import {
   Result,
   isErrorResult,
+  makeErrorResult,
   makeSuccessResult,
   unwrapResult,
 } from "shared/utils/result";
@@ -87,9 +91,19 @@ export const runAssignmentStrategy = (
   if (assignmentStrategy === AssignmentStrategy.GROUP_PADG) {
     const groupResultResult = groupAssignPlayers(players, games, startingTime);
     if (isErrorResult(groupResultResult)) {
-      return groupResultResult;
+      logger.error(
+        "%s",
+        new Error(`Group assignment failed: ${groupResultResult.error}`)
+      );
     }
-    const groupResult = unwrapResult(groupResultResult);
+    const groupResult = isErrorResult(groupResultResult)
+      ? {
+          results: [],
+          message: `Random assignment failed: ${groupResultResult.error}`,
+          algorithm: AssignmentStrategy.GROUP,
+          status: AssignmentResultStatus.ERROR,
+        }
+      : unwrapResult(groupResultResult);
 
     const padgResultResult = padgAssignPlayers(
       players,
@@ -98,19 +112,40 @@ export const runAssignmentStrategy = (
       signups
     );
     if (isErrorResult(padgResultResult)) {
-      return padgResultResult;
+      logger.error(
+        "%s",
+        new Error(`PADG assignment failed: ${padgResultResult.error}`)
+      );
     }
+    const padgResult = isErrorResult(padgResultResult)
+      ? {
+          results: [],
+          message: `PADG assignment failed: ${padgResultResult.error}`,
+          algorithm: AssignmentStrategy.PADG,
+          status: AssignmentResultStatus.ERROR,
+        }
+      : unwrapResult(padgResultResult);
 
-    const padgResult = unwrapResult(padgResultResult);
+    if (isErrorResult(groupResultResult) && isErrorResult(padgResultResult)) {
+      logger.error(
+        "%s",
+        new Error(`Both group and PADG assignments failed, stop assignment`)
+      );
+      return makeErrorResult(AssignmentError.UNKNOWN_ERROR);
+    }
 
     logger.info(
       `Group result: ${groupResult.results.length} players, Padg result: ${padgResult.results.length} players`
     );
 
-    if (groupResult.results.length > padgResult.results.length) {
+    if (
+      padgResult.status === AssignmentResultStatus.ERROR ||
+      groupResult.results.length > padgResult.results.length
+    ) {
       logger.info("----> Use Group Assign result");
       return makeSuccessResult(groupResult);
     }
+
     logger.info("----> Use Padg Assign result");
     return makeSuccessResult(padgResult);
   }
@@ -123,9 +158,20 @@ export const runAssignmentStrategy = (
       signups
     );
     if (isErrorResult(randomResultResult)) {
-      return randomResultResult;
+      logger.error(
+        "%s",
+        new Error(`Random assignment failed: ${randomResultResult.error}`)
+      );
     }
-    const randomResult = unwrapResult(randomResultResult);
+    const randomResult = isErrorResult(randomResultResult)
+      ? {
+          results: [],
+          message: `Random assignment failed: ${randomResultResult.error}`,
+          algorithm: AssignmentStrategy.RANDOM,
+          status: AssignmentResultStatus.ERROR,
+        }
+      : unwrapResult(randomResultResult);
+
     const padgResultResult = padgAssignPlayers(
       players,
       games,
@@ -133,16 +179,40 @@ export const runAssignmentStrategy = (
       signups
     );
     if (isErrorResult(padgResultResult)) {
-      return padgResultResult;
+      logger.error(
+        "%s",
+        new Error(`PADG assignment failed: ${padgResultResult.error}`)
+      );
     }
-    const padgResult = unwrapResult(padgResultResult);
+    const padgResult = isErrorResult(padgResultResult)
+      ? {
+          results: [],
+          message: `PADG assignment failed: ${padgResultResult.error}`,
+          algorithm: AssignmentStrategy.PADG,
+          status: AssignmentResultStatus.ERROR,
+        }
+      : unwrapResult(padgResultResult);
+
+    if (isErrorResult(randomResultResult) && isErrorResult(padgResultResult)) {
+      logger.error(
+        "%s",
+        new Error(`Both random and PADG assignments failed, stop assignment`)
+      );
+      return makeErrorResult(AssignmentError.UNKNOWN_ERROR);
+    }
+
     logger.info(
       `Random result: ${randomResult.results.length} players, Padg result: ${padgResult.results.length} players`
     );
-    if (randomResult.results.length > padgResult.results.length) {
+
+    if (
+      padgResult.status === AssignmentResultStatus.ERROR ||
+      randomResult.results.length > padgResult.results.length
+    ) {
       logger.info("----> Use Random assign result");
       return makeSuccessResult(randomResult);
     }
+
     logger.info("----> Use Padg Assign result");
     return makeSuccessResult(padgResult);
   }
