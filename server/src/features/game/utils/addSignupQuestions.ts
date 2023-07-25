b62f8cb9faf9f674be9ae5d1102c9c0a6110483a
Arttu Hanska <arttu.hanska@gmail.com>
2023-07-25 22:08:34 +0300

    Add signup questions to config (#718)

---
+++ server/src/features/game/utils/addSignupQuestions.ts
@@ -0,0 +1,43 @@
+import { findGames } from "server/features/game/gameRepository";
+import { saveSignupQuestion } from "server/features/settings/settingsRepository";
+import { logger } from "server/utils/logger";
+import { sharedConfig } from "shared/config/sharedConfig";
+import { ProgramType } from "shared/typings/models/game";
+import { isErrorResult, unwrapResult } from "shared/utils/result";
+
+export const addSignupQuestions = async (): Promise<void> => {
+  const {
+    signupQuestions,
+    tournamentSignupQuestion,
+    tournamentSignupQuestionExcludeIds,
+  } = sharedConfig;
+
+  const questionPromises = signupQuestions.map(async (signupQuestion) => {
+    await saveSignupQuestion(signupQuestion);
+  });
+  await Promise.all(questionPromises);
+
+  const gamesResult = await findGames();
+  if (isErrorResult(gamesResult)) {
+    logger.error(
+      "%s",
+      new Error(`Error finding games: ${unwrapResult(gamesResult)}`)
+    );
+    return;
+  }
+  const games = unwrapResult(gamesResult);
+
+  const tournaments = games
+    .filter((game) => game.programType === ProgramType.TOURNAMENT)
+    .filter(
+      (game) => !tournamentSignupQuestionExcludeIds.includes(game.gameId)
+    );
+
+  const tournamentPromises = tournaments.map(async (tournament) => {
+    await saveSignupQuestion({
+      ...tournamentSignupQuestion,
+      gameId: tournament.gameId,
+    });
+  });
+  await Promise.all(tournamentPromises);
+};
