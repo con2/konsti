1120b1c9117b5b56738219ae1918445ec67fae9c
Arttu Hanska <arttu.hanska@gmail.com>
2023-07-25 23:07:29 +0300

    Build short description from long description (#720)

---
+++ client/src/utils/getShortDescriptionFromDescription.ts
@@ -0,0 +1,20 @@
+const SHORT_DESCRIPTION_MAX_LENGTH = 200;
+const matchNextSentence = /([.?!])\s*(?=[A-Z])/g;
+
+export const getShortDescriptionFromDesctiption = (
+  description: string
+): string => {
+  let shortDescription = "";
+
+  const descriptionArray = description
+    .replace(matchNextSentence, "$1|")
+    .split("|");
+
+  for (const value of descriptionArray) {
+    shortDescription = shortDescription.concat(`${value} `);
+    if (shortDescription.length >= SHORT_DESCRIPTION_MAX_LENGTH) {
+      break;
+    }
+  }
+  return shortDescription;
+};
