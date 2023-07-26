5199b369be4d1cc3e25cc79adb3addf04185cd75
Arttu Hanska <arttu.hanska@gmail.com>
2023-07-26 19:21:08 +0300

    Add todo comment

--- server/src/features/player-assignment/utils/removeOverlapSignups.ts
+++ server/src/features/player-assignment/utils/removeOverlapSignups.ts
@@ -14,6 +14,7 @@ import {
 } from "shared/utils/result";
 import { MongoDbError } from "shared/typings/api/errors";
 
+// TODO: Can this be deleted?
 dayjs.extend(isBetween);
 
 export const removeOverlapSignups = async (
