0dc96c99936f813984f5adc38abb0486fe3e4085
Arttu Hanska <arttu.hanska@gmail.com>
2023-09-17 15:57:22 +0300

    Fix null value handling with Result

--- shared/utils/result.ts
+++ shared/utils/result.ts
@@ -1,3 +1,5 @@
+type NonUndefined<T> = Exclude<T, undefined>; // Remove undefined from T
+
 interface SuccessResult<T> {
   value: T;
   error?: never;
@@ -8,9 +10,9 @@ interface ErrorResult<Err> {
   value?: never;
 }
 
-export type Result<T, Err> = NonNullable<SuccessResult<T> | ErrorResult<Err>>;
+export type Result<T, Err> = NonUndefined<SuccessResult<T> | ErrorResult<Err>>;
 
-type UnwrapResult = <T, Err>(e: Result<T, Err>) => NonNullable<T | Err>;
+type UnwrapResult = <T, Err>(e: Result<T, Err>) => NonUndefined<T | Err>;
 
 // https://antman-does-software.com/stop-catching-errors-in-typescript-use-the-either-type-to-make-your-code-predictable
 export const unwrapResult: UnwrapResult = <T, Err>({
@@ -26,10 +28,10 @@ export const unwrapResult: UnwrapResult = <T, Err>({
     );
   }
   if (value !== undefined) {
-    return value as NonNullable<T>;
+    return value as NonUndefined<T>;
   }
   if (error !== undefined) {
-    return error as NonNullable<Err>; // Typescript is getting confused and returning this type as `T | undefined` unless we add the type assertion
+    return error as NonUndefined<Err>; // Typescript is getting confused and returning this type as `T | undefined` unless we add the type assertion
   }
   // eslint-disable-next-line no-restricted-syntax -- This is some kind of runtime error we want to catch
   throw new Error(`Received no value or error at runtime when opening Result`);
