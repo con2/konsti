4de4cf86434d26b76d99777bfc1585c9167c1c6b
Arttu Hanska <arttu.hanska@gmail.com>
2023-09-24 21:13:45 +0300

    Improve UI texts (#769)

--- client/src/views/all-games/components/RevolvingDoorGamesInfo.tsx
+++ client/src/views/all-games/components/RevolvingDoorGamesInfo.tsx
@@ -39,7 +39,7 @@ export const RevolvingDoorGamesInfo = (): ReactElement => {
       {/* eslint-disable-next-line @typescript-eslint/no-unnecessary-condition */}
       {!runningRevolvingDoorGames || runningRevolvingDoorGames.length === 0 ? (
         <NoGamesInfoText>
-          {t("noCurrentlyRunningGames", {
+          {t("noRunningRevolvingDoorProgramItems", {
             PROGRAM_TYPE: t(`programTypePartitivePlural.${activeProgramType}`),
           })}
         </NoGamesInfoText>
