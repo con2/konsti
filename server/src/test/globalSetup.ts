dc8075d0f49f856db10f3333f10f14a57c70ff87
Arttu Hanska <arttu.hanska@gmail.com>
2023-11-10 21:29:53 +0200

    Remove unnecessary line

--- server/src/test/globalSetup.ts
+++ server/src/test/globalSetup.ts
@@ -5,8 +5,7 @@ let mongoDb: MongoMemoryServer;
 // eslint-disable-next-line import/no-unused-modules
 export const setup = async (): Promise<void> => {
   process.env.MONGOMS_VERSION = "6.0.10";
-  mongoDb = await MongoMemoryServer.create({ instance: { port: 57233 } });
-  globalThis.__MONGO_URI__ = mongoDb.getUri(); // mongodb://127.0.0.1:57233/
+  mongoDb = await MongoMemoryServer.create({ instance: { port: 57233 } }); // mongodb://127.0.0.1:57233/
 };
 
 // eslint-disable-next-line import/no-unused-modules
