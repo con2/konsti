11c49fc20ad5afa56b7abfd328844c4e318a515e
Arttu Hanska <arttu.hanska@gmail.com>
2023-11-10 21:37:13 +0200

    Second attempt at using single mongodb-memory-server (#822)

--- server/src/test/global.d.ts
+++ server/src/test/global.d.ts
@@ -1,5 +1,2 @@
 // eslint-disable-next-line no-var
 declare var __MONGO_URI__: string;
-
-// eslint-disable-next-line no-var
-declare var __MONGO_DB__: import("mongodb-memory-server").MongoMemoryServer;
--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
+    globalSetup: ["./server/src/test/globalSetup.ts"],
     setupFiles: ["./src/test/setupTests.ts"],
     testTimeout: 30 * 1000,
     hookTimeout: 60 * 1000,
