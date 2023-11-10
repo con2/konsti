f4114a05a10b0bc581393aa4410f66d3df874614
Arttu Hanska <arttu.hanska@gmail.com>
2023-11-10 21:21:12 +0200

    Second attempt at using single mongodb-memory-server

---
+++ server/src/test/globalSetup.ts
@@ -0,0 +1,15 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+let mongoDb: MongoMemoryServer;
+
+// eslint-disable-next-line import/no-unused-modules
+export const setup = async (): Promise<void> => {
+  process.env.MONGOMS_VERSION = "6.0.10";
+  mongoDb = await MongoMemoryServer.create({ instance: { port: 57233 } });
+  globalThis.__MONGO_URI__ = mongoDb.getUri(); // mongodb://127.0.0.1:57233/
+};
+
+// eslint-disable-next-line import/no-unused-modules
+export const teardown = async (): Promise<void> => {
+  await mongoDb.stop();
+};
--- server/src/test/setupTests.ts
+++ server/src/test/setupTests.ts
@@ -1,11 +1,7 @@
-import { afterAll, beforeAll, vi } from "vitest";
+import { vi } from "vitest";
 import { initializeDayjs } from "shared/utils/initializeDayjs";
 import { config } from "shared/config";
 import { ProgramType } from "shared/typings/models/game";
-import {
-  setupMongoDbMemoryServer,
-  teardownMongoDbMemoryServer,
-} from "server/test/mongoDdMemoryServerSetup";
 
 initializeDayjs();
 
@@ -23,7 +19,8 @@ if (!config.server().enableLoggingInTests) {
   });
 }
 
-process.env.MONGOMS_VERSION = "6.0.10";
+// Defined in globalSetup.ts
+globalThis.__MONGO_URI__ = "mongodb://127.0.0.1:57233/";
 
 vi.spyOn(config, "shared").mockReturnValue({
   ...config.shared(),
@@ -31,12 +28,4 @@ vi.spyOn(config, "shared").mockReturnValue({
   conventionEndTime: "2023-07-30T21:00:00Z", // Sun 24:00 GMT+3
   directSignupAlwaysOpenIds: ["1234"],
   twoPhaseSignupProgramTypes: [ProgramType.TABLETOP_RPG, ProgramType.LARP],
-});
-
-beforeAll(async () => {
-  await setupMongoDbMemoryServer();
-});
-
-afterAll(async () => {
-  await teardownMongoDbMemoryServer();
 });
