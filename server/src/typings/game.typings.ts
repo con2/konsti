80c61858bf4cce943957c8d449ccfa14dcc8a58a
Arttu Hanska <arttu.hanska@gmail.com>
2023-11-26 22:08:16 +0200

    Fix eslint errors

--- server/src/typings/game.typings.ts
+++ server/src/typings/game.typings.ts
@@ -2,8 +2,3 @@ import mongoose from "mongoose";
 import { Game } from "shared/typings/models/game";
 
 export interface GameDoc extends Game, mongoose.Document {}
-
-export interface GameWithPlayerCount {
-  game: Game;
-  players: number;
-}
--- server/src/typings/groupAssign.typings.ts
+++
@@ -1,8 +0,0 @@
-import { AssignmentResult } from "shared/typings/models/result";
-
-export interface GroupAssignResult {
-  score: number;
-  signupResults: readonly AssignmentResult[];
-  playerCounter: number;
-  gameCounter: number;
-}
