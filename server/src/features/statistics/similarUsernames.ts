7d89de88823cb268aaa809ada9758e22e00c07d9
Arttu Hanska <arttu.hanska@gmail.com>
2023-12-09 13:39:07 +0200

    Add script to detect similar usernames (#844)

---
+++ server/src/features/statistics/similarUsernames.ts
@@ -0,0 +1,47 @@
+import fs from "fs";
+import { distance, closest } from "fastest-levenshtein";
+import { logger } from "server/utils/logger";
+import { User } from "shared/types/models/user";
+import { config } from "shared/config";
+
+export const getSimilarUsernames = (year: number, event: string): void => {
+  const users: User[] = JSON.parse(
+    fs.readFileSync(
+      `${config.server().statsDataDir}/${event}/${year}/secret/users.json`,
+      "utf8",
+    ),
+  );
+
+  const usernames = users.map((user) => user.username.toLowerCase());
+
+  const results = usernames.flatMap((username, index) => {
+    const lowercaseUsername = username.toLowerCase();
+
+    const usernamesWithoutCurrent = usernames
+      .toSpliced(index, 1)
+      .filter((comparedUsername) => {
+        // Don't include usernames where two first letters are same
+        return (
+          comparedUsername.startsWith(lowercaseUsername[0]) &&
+          comparedUsername[1] === lowercaseUsername[1]
+        );
+      });
+
+    const closestSimilarUsername = closest(username, usernamesWithoutCurrent);
+    if (!closestSimilarUsername) {
+      return [];
+    }
+    const usernameDistance = distance(username, closestSimilarUsername);
+
+    return {
+      username,
+      closest: closestSimilarUsername,
+      distance: usernameDistance,
+    };
+  });
+
+  const similarUsernames = results.filter((result) => result.distance === 1);
+
+  logger.info(JSON.stringify(similarUsernames, null, 2));
+  logger.info(`Found ${similarUsernames.length} similar usernames`);
+};
