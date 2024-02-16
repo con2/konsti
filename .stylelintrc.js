3c891e5ff6225b39794c90c2f8e14c72c1570cda
Arttu Hanska <arttu.hanska@gmail.com>
2024-02-16 20:02:44 +0200

    Fix stylelint-a11y rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
-  plugins: ["stylelint-no-unsupported-browser-features"],
+  plugins: [
+    "stylelint-no-unsupported-browser-features",
+    "@double-great/stylelint-a11y",
+  ],
 
   extends: [
     "stylelint-config-recommended",
-    // TODO: Figure out why this doesn't work
+    // Recommended config doesn't work: https://github.com/double-great/stylelint-a11y/issues/65
     // "@double-great/stylelint-a11y/recommended",
   ],
 
@@ -19,6 +22,12 @@ module.exports = {
     // stylelint
     "length-zero-no-unit": true,
     "media-query-no-invalid": null, // Doesn't work with styled-components
+
+    // @double-great/stylelint-a11y
+    // Same rules as in recommended config - enable single rules since config doesn't work
+    "a11y/media-prefers-reduced-motion": true,
+    "a11y/no-outline-none": true,
+    "a11y/selector-pseudo-class-focus": true,
 
     // no-unsupported-browser-features
     "plugin/no-unsupported-browser-features": [
