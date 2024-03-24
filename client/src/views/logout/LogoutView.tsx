854cadaff29e7f27f274c29a1d67428c4e1d9f5f
Arttu Hanska <arttu.hanska@gmail.com>
2024-03-24 21:14:54 +0200

    Use local logout if admin

--- client/src/views/logout/LogoutView.tsx
+++ client/src/views/logout/LogoutView.tsx
@@ -5,10 +5,12 @@ import { useAppDispatch, useAppSelector } from "client/utils/hooks";
 import { postKompassiLogoutRedirect } from "client/services/loginServices";
 import { LoginProvider } from "shared/config/sharedConfigTypes";
 import { Loading } from "client/components/Loading";
+import { UserGroup } from "shared/types/models/user";
 
 export const LogoutView = (): ReactElement => {
   const dispatch = useAppDispatch();
   const loginProvider = useAppSelector((state) => state.admin.loginProvider);
+  const userGroup = useAppSelector((state) => state.login.userGroup);
   const isLocalLogin = loginProvider === LoginProvider.LOCAL;
 
   useEffect(() => {
@@ -16,12 +18,12 @@ export const LogoutView = (): ReactElement => {
       await postKompassiLogoutRedirect();
     };
 
-    if (isLocalLogin) {
+    if (isLocalLogin || userGroup === UserGroup.ADMIN) {
       dispatch(submitLogout());
     } else {
       doKompassiLogout();
     }
-  }, [dispatch, isLocalLogin]);
+  }, [dispatch, isLocalLogin, userGroup]);
 
   return (
     <>
