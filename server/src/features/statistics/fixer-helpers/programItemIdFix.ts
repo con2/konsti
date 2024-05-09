767706325b8ce8a25434ebfd3565826bc0b81ad6
Arttu Hanska <arttu.hanska@gmail.com>
2024-05-09 14:41:32 +0300

    Add missing file

---
+++ server/src/features/statistics/fixer-helpers/programItemIdFix.ts
@@ -0,0 +1,161 @@
+import fs from "fs";
+import { isEqual } from "lodash-es";
+import { logger } from "server/utils/logger";
+import { User } from "shared/types/models/user";
+import { ProgramItemDoc } from "server/types/programItemTypes";
+import { ResultsCollectionEntry } from "server/types/resultTypes";
+import { writeJson } from "server/features/statistics/statsUtil";
+import { config } from "shared/config";
+import { ProgramItem } from "shared/types/models/programItem";
+import { DirectSignupDoc } from "server/features/direct-signup/directSignupTypes";
+import { SettingsDoc } from "server/types/settingsTypes";
+
+export const programItemIdFix = async (
+  year: number,
+  event: string,
+): Promise<void> => {
+  const users: User[] = JSON.parse(
+    fs.readFileSync(
+      `${config.server().statsDataDir}/${event}/${year}/users.json`,
+      "utf8",
+    ),
+  );
+
+  logger.info(`Loaded ${users.length} users`);
+
+  const results: ResultsCollectionEntry[] = JSON.parse(
+    fs.readFileSync(
+      `${config.server().statsDataDir}/${event}/${year}/results.json`,
+      "utf8",
+    ),
+  );
+
+  logger.info(`Loaded ${results.length} results`);
+
+  const games: ProgramItemDoc[] = JSON.parse(
+    fs.readFileSync(
+      `${config.server().statsDataDir}/${event}/${year}/games.json`,
+      "utf8",
+    ),
+  );
+
+  logger.info(`Loaded ${games.length} games`);
+
+  const directSignups: DirectSignupDoc[] = JSON.parse(
+    fs.readFileSync(
+      `${config.server().statsDataDir}/${event}/${year}/direct-signups.json`,
+      "utf8",
+    ),
+  );
+
+  logger.info(`Loaded ${games.length} games`);
+
+  const settings: SettingsDoc[] = JSON.parse(
+    fs.readFileSync(
+      `${config.server().statsDataDir}/${event}/${year}/settings.json`,
+      "utf8",
+    ),
+  );
+
+  logger.info(`Loaded ${settings.length} games`);
+
+  users.map((user) => {
+    const tempFavoritedGames = user.favoritedGames.map((favoritedGame) => {
+      const matchingGame = games.find(
+        // @ts-expect-error: $oid not in interface
+        (game) => game._id.$oid === favoritedGame.$oid,
+      );
+      if (!matchingGame) {
+        logger.error(
+          `Favorited: program item for id ${JSON.stringify(
+            favoritedGame,
+          )} not found`,
+        );
+        return { programItemId: "<canceled>" };
+      }
+      return { programItemId: matchingGame.programItemId };
+    });
+
+    const tempLotterySignups = user.lotterySignups.map((lotterySignup) => {
+      const matchingGame = games.find(
+        // @ts-expect-error: $oid not in interface
+        (game) => game._id.$oid === lotterySignup.programItemDetails.$oid,
+      );
+      if (!matchingGame) {
+        logger.error(
+          `Lottery signup: program item for id ${JSON.stringify(lotterySignup)} not found`,
+        );
+        return {
+          ...lotterySignup,
+          programItemDetails: { programItemId: "<canceled>" },
+        };
+      }
+      return {
+        ...lotterySignup,
+        programItemDetails: { programItemId: matchingGame.programItemId },
+      };
+    });
+
+    // @ts-expect-error: We don't want whole game details
+    user.favoritedGames = tempFavoritedGames;
+    // @ts-expect-error: We don't want whole game details
+    user.lotterySignups = tempLotterySignups;
+  });
+
+  results.map((result) => {
+    result.results.map((userResult) => {
+      const matchingGame = games.find((game) => {
+        return isEqual(game._id, userResult.directSignup.programItemDetails);
+      });
+
+      if (!matchingGame) {
+        logger.error(
+          `Results: program item for id ${JSON.stringify(
+            userResult.directSignup.programItemDetails,
+          )} not found`,
+        );
+        userResult.directSignup = {
+          ...userResult.directSignup,
+          // @ts-expect-error: We don't want whole game details
+          programItemDetails: { programItemId: "<canceled>" },
+        };
+        return;
+      }
+
+      userResult.directSignup = {
+        ...userResult.directSignup,
+        // @ts-expect-error: We don't want whole game details
+        programItemDetails: { programItemId: matchingGame.programItemId },
+      };
+    });
+  });
+
+  directSignups.map((signup) => {
+    games.map((game) => {
+      if (isEqual(game._id, signup.game)) {
+        // @ts-expect-error: We don't want whole game details
+        signup.game = { programItemId: game.programItemId };
+      }
+    });
+  });
+
+  const tempHiddenGames: ProgramItem[] = [];
+
+  settings.map((setting) => {
+    games.map((game) => {
+      setting.hiddenGames.map((hiddenGame) => {
+        if (isEqual(game._id, hiddenGame)) {
+          // @ts-expect-error: We don't want whole game details
+          tempHiddenGames.push({ programItemId: game.programItemId });
+        }
+      });
+    });
+  });
+
+  settings[0].hiddenGames = tempHiddenGames;
+
+  await writeJson(year, event, "users", users);
+  await writeJson(year, event, "results", results);
+  await writeJson(year, event, "signups", directSignups);
+  await writeJson(year, event, "settings", settings);
+};
