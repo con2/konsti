1d75c526309cc83115aced381b5471aea4256970
Arttu Hanska <arttu.hanska@gmail.com>
2024-07-07 14:47:58 +0300

    Add test function getPreviousDirectSignup

--- server/src/features/assignment/utils/getList.test.ts
+++ server/src/features/assignment/utils/getList.test.ts
@@ -9,56 +9,56 @@ import { ProgramType } from "shared/types/models/programItem";
 const startTime = "2019-11-23T12:00:00+02:00";
 const groupCreatorGroupCode = "123-234-345";
 
-const groupCreatorWithLotterySignups: User = {
-  kompassiId: 0,
-  kompassiUsernameAccepted: false,
-  username: "username",
-  password: "password",
-  userGroup: UserGroup.USER,
-  serial: "123",
-  groupCode: groupCreatorGroupCode,
-  groupCreatorCode: groupCreatorGroupCode,
-  favoriteProgramItemIds: [],
-  lotterySignups: [
-    {
-      programItem: testProgramItem,
-      priority: 1,
-      time: startTime,
-      message: "",
-    },
-  ],
-  createdAt: startTime,
-  eventLogItems: [],
-};
+const getUsers = ({
+  count,
+  noLotterySignups = false,
+}: {
+  count: number;
+  noLotterySignups?: boolean;
+}): User[] => {
+  const users: User[] = [];
 
-const groupMemberWithoutLotterySignups1: User = {
-  kompassiId: 0,
-  kompassiUsernameAccepted: false,
-  username: "username 2",
-  password: "password",
-  userGroup: UserGroup.USER,
-  serial: "456",
-  groupCode: groupCreatorGroupCode,
-  groupCreatorCode: "0",
-  favoriteProgramItemIds: [],
-  lotterySignups: [],
-  createdAt: startTime,
-  eventLogItems: [],
-};
+  for (let i = 0; i < count; i++) {
+    const defaultUserValues = {
+      kompassiId: 0,
+      kompassiUsernameAccepted: false,
+      password: "password",
+      userGroup: UserGroup.USER,
+      serial: `12${i}`,
+      groupCode: groupCreatorGroupCode,
+      favoriteProgramItemIds: [],
+      createdAt: dayjs(startTime).subtract(4, "hours").toISOString(),
+      eventLogItems: [],
+    };
 
-const groupMemberWithoutLotterySignups2: User = {
-  kompassiId: 0,
-  kompassiUsernameAccepted: false,
-  username: "username 3",
-  password: "password",
-  userGroup: UserGroup.USER,
-  serial: "789",
-  groupCode: groupCreatorGroupCode,
-  groupCreatorCode: "0",
-  favoriteProgramItemIds: [],
-  lotterySignups: [],
-  createdAt: startTime,
-  eventLogItems: [],
+    if (i === 0) {
+      users.push({
+        ...defaultUserValues,
+        username: "group-creator-with-lottery-signup",
+        groupCreatorCode: groupCreatorGroupCode,
+        lotterySignups: noLotterySignups
+          ? []
+          : [
+              {
+                programItem: testProgramItem,
+                priority: 1,
+                time: startTime,
+                message: "",
+              },
+            ],
+      });
+      continue;
+    }
+
+    users.push({
+      ...defaultUserValues,
+      username: `group-member-${i}`,
+      groupCreatorCode: "0",
+      lotterySignups: [],
+    });
+  }
+
+  return users;
 };
 
 const getPreviousDirectSignup = ({
@@ -85,7 +85,7 @@ const getPreviousDirectSignup = ({
 };
 
 test("should return empty array if user has no lottery signups", () => {
-  const userArray: User[] = [groupMemberWithoutLotterySignups2];
+  const userArray: User[] = getUsers({ count: 1, noLotterySignups: true });
   const attendeeGroups: readonly User[][] = [userArray, userArray, userArray];
   const list = getList(attendeeGroups, startTime, []);
 
@@ -93,7 +93,7 @@ test("should return empty array if user has no lottery signups", () => {
 });
 
 test("should generate assignment list with bonuses for single user without any direct signups", () => {
-  const userArray: User[] = [groupCreatorWithLotterySignups];
+  const userArray: User[] = getUsers({ count: 1 });
   const attendeeGroups: readonly User[][] = [userArray, userArray, userArray];
   const list = getList(attendeeGroups, startTime, []);
 
@@ -122,7 +122,7 @@ test("should generate assignment list with bonuses for single user without any d
 });
 
 test("should generate assignment list with bonuses for single user without previous direct signups", () => {
-  const userArray: User[] = [groupCreatorWithLotterySignups];
+  const userArray: User[] = getUsers({ count: 1 });
   const attendeeGroups: readonly User[][] = [userArray, userArray, userArray];
   const list = getList(attendeeGroups, startTime, [
     getPreviousDirectSignup({ username: "foobar user" }),
@@ -153,10 +153,7 @@ test("should generate assignment list with bonuses for single user without previ
 });
 
 test("should generate assignment list with bonuses for group without previous direct signups", () => {
-  const userArray: User[] = [
-    groupCreatorWithLotterySignups,
-    groupMemberWithoutLotterySignups1,
-  ];
+  const userArray: User[] = getUsers({ count: 2 });
   const attendeeGroups: readonly User[][] = [userArray, userArray, userArray];
   const list = getList(attendeeGroups, startTime, [
     getPreviousDirectSignup({ username: "foobar user" }),
@@ -187,14 +184,11 @@ test("should generate assignment list with bonuses for group without previous di
 });
 
 test("should generate assignment list without bonuses for group with previous direct signups", () => {
-  const userArray: User[] = [
-    groupCreatorWithLotterySignups,
-    groupMemberWithoutLotterySignups2,
-  ];
+  const userArray: User[] = getUsers({ count: 2 });
   const attendeeGroups: readonly User[][] = [userArray, userArray, userArray];
   const list = getList(attendeeGroups, startTime, [
     getPreviousDirectSignup({
-      username: groupMemberWithoutLotterySignups2.username,
+      username: userArray[1].username,
     }),
   ]);
 
@@ -223,12 +217,12 @@ test("should generate assignment list without bonuses for group with previous di
 });
 
 test("should generate assignment list with bonuses if user has direct signups for different program type", () => {
-  const userArray: User[] = [groupCreatorWithLotterySignups];
+  const userArray: User[] = getUsers({ count: 1 });
   const attendeeGroups: readonly User[][] = [userArray, userArray, userArray];
   const list = getList(attendeeGroups, startTime, [
     getPreviousDirectSignup({
       programType: ProgramType.TOURNAMENT,
-      username: groupCreatorWithLotterySignups.username,
+      username: userArray[0].username,
     }),
   ]);
 
