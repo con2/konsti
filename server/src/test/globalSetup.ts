6b958a4165c17651043f9e74398590bac40a2469
Arttu Hanska <arttu.hanska@gmail.com>
2024-07-18 13:57:05 +0300

    Update MongoDB to 7.0.12 (#996)

--- server/src/test/globalSetup.ts
+++ server/src/test/globalSetup.ts
@@ -4,7 +4,7 @@ let mongoDb: MongoMemoryServer;
 
 // eslint-disable-next-line import/no-unused-modules
 export const setup = async (): Promise<void> => {
-  process.env.MONGOMS_VERSION = "7.0.11";
+  process.env.MONGOMS_VERSION = "7.0.12";
   mongoDb = await MongoMemoryServer.create({ instance: { port: 57233 } }); // mongodb://127.0.0.1:57233/
 };
 
