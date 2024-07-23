d3e0639c44f83e48b0423130dd6a7c8ac6c23d58
Arttu Hanska <arttu.hanska@gmail.com>
2024-07-23 14:42:11 +0300

    Disable cronjobs (#1002)

--- shared/config/serverConfig.ts
+++ shared/config/serverConfig.ts
@@ -98,14 +98,14 @@ const prodConfig = {
   useTestTime: false,
 
   // Program update cron
-  autoUpdateProgramEnabled: true,
+  autoUpdateProgramEnabled: false,
   programUpdateInterval: `5,10,20,25,35,40,50,55 * * * *`,
 
   // Program update
-  updateProgramItemPopularityEnabled: true,
+  updateProgramItemPopularityEnabled: false,
 
   // Attendee assign cron
-  autoAssignAttendeesEnabled: true,
+  autoAssignAttendeesEnabled: false,
   autoAssignInterval: `0,15,30,45 * * * *`,
   autoAssignDelay: 1000 * 5,
 };
@@ -126,14 +126,14 @@ const stagingConfig = {
   useTestTime: true,
 
   // Program update cron
-  autoUpdateProgramEnabled: true,
+  autoUpdateProgramEnabled: false,
   programUpdateInterval: `5,10,20,25,35,40,50,55 * * * *`,
 
   // Program update
-  updateProgramItemPopularityEnabled: true,
+  updateProgramItemPopularityEnabled: false,
 
   // Attendee assign cron
-  autoAssignAttendeesEnabled: true,
+  autoAssignAttendeesEnabled: false,
   autoAssignInterval: `0,15,30,45 * * * *`,
   autoAssignDelay: 1000 * 5,
 };
