86afbfd9d088377f3916ba66075c5ee4d05ab176
Arttu Hanska <arttu.hanska@gmail.com>
2024-09-02 21:16:39 +0300

    Disable enableRemoveOverlapSignups for Tracon 2024 (#1051)
    * Disable enableRemoveOverlapSignups for Tracon 2024
    
    * Fix test

--- shared/config/eventConfigTypes.ts
+++ shared/config/eventConfigTypes.ts
@@ -74,4 +74,5 @@ export interface EventConfig {
   directSignupOpenToEndProgramTypes: ProgramType[];
   activeProgramTypes: ProgramType[];
   popularityAlgorithm: AssignmentAlgorithm.RANDOM | AssignmentAlgorithm.PADG;
+  enableRemoveOverlapSignups: boolean;
 }
