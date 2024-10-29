261e9e74ee4433b04f9783d28a16755a63dce296
Arttu Hanska <arttu.hanska@gmail.com>
2024-10-29 20:37:39 +0200

    Update config

--- shared/config/past-events/tracon2024.ts
+++ shared/config/past-events/tracon2024.ts
@@ -11,7 +11,7 @@ const friday = "2024-09-06";
 // const saturday = "2024-09-07";
 // const sunday = "2024-09-07";
 
-export const eventConfig: EventConfig = {
+export const eventConfig: Partial<EventConfig> = {
   // Event info
   eventName: EventName.TRACON,
   eventYear: "2024",
