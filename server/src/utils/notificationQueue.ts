ab4ce459a4a8de3977c56c4d2e2511ccd71b69cf
user <user@example.com>
2025-04-12 19:56:04 +0300

    Add notificationQueue.ts

---
+++ server/src/utils/notificationQueue.ts
@@ -0,0 +1,55 @@
+import * as fastq from "fastq";
+import type { queueAsPromised } from "fastq";
+import { QueueError } from "shared/types/api/errors";
+import { makeErrorResult, makeSuccessResult, Result } from "shared/utils/result";
+
+export enum NotificationTaskType {
+    SEND_EMAIL_ACCEPTED,
+    SEND_EMAIL_REJECTED
+}
+
+export type NotificationTask = {
+  type: NotificationTaskType
+  username: string
+}
+
+let queue: queueAsPromised<NotificationTask>
+
+export async function addNotificationsBulk(notifications: NotificationTask[]): Promise<Result<boolean, QueueError>> {
+    if (notifications.length == 0) {
+        return makeSuccessResult(true)
+    }
+
+    try {
+        notifications.forEach(addNotification)
+        return makeSuccessResult(true)
+    } catch {
+        return makeErrorResult(QueueError.FAILED_TO_PUSH)
+    }
+}
+
+export async function addNotification(notification: NotificationTask): Promise<Result<boolean, QueueError>> {
+    if (!queue) {
+        return makeErrorResult(QueueError.QUEUE_NOT_INITIALIZED)
+    }
+    
+    try {
+        await queue.push(notification)
+        return makeSuccessResult(true)
+    } catch {
+        return makeErrorResult(QueueError.FAILED_TO_PUSH)
+    }
+}
+
+export function setupEmailNotificationQueue(workerCount: number = 1): queueAsPromised<NotificationTask>   {
+    queue = fastq.promise(asyncWorker, workerCount)
+    return queue
+}
+
+export function getQueue(): queueAsPromised<NotificationTask> {
+    return queue
+}
+
+export async function asyncWorker (arg: NotificationTask): Promise<void> {
+    console.log(arg.type)
+}
