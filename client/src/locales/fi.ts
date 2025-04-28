f20170a25156d7829da864b0cf803b2a6ba8bad5
Arttu Hanska <arttu.hanska@gmail.com>
2025-04-28 20:52:28 +0300

    Improve text

--- client/src/locales/fi.ts
+++ client/src/locales/fi.ts
@@ -523,7 +523,7 @@ export const translationFI = {
     showMore: "Näytä lisää",
     showLess: "Näytä vähemmän",
     inProgress:
-      "Kaikki tiedot eivät ole ajantasalla vielä ja sisältö päivittyy",
+      "Kaikki tiedot eivät ole vielä ajantasalla ja sisältö päivittyy",
   },
   profileView: {
     profileTab: "Profiili",
