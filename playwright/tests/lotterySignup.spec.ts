416dbf2167b6fab3f64b3811513e666e927242ed
Arttu Hanska <arttu.hanska@gmail.com>
2025-07-20 22:15:03 +0300

    Add e2e test with multiple lottery program types

--- playwright/tests/lotterySignup.spec.ts
+++ playwright/tests/lotterySignup.spec.ts
@@ -11,6 +11,11 @@ import {
 } from "playwright/playwrightUtils";
 import { EventSignupStrategy } from "shared/config/eventConfigTypes";
 import { config } from "shared/config";
+import {
+  testProgramItem,
+  testProgramItem2,
+} from "shared/tests/testProgramItem";
+import { ProgramType } from "shared/types/models/programItem";
 
 test("Add lottery signup", async ({ page, request }) => {
   await populateDb(request, { clean: true, users: true, admin: true });
@@ -179,5 +184,92 @@ test("Did not receive spot in lottery signup", async ({ page, request }) => {
   await page.getByRole("link", { name: "Show all notifications" }).click();
   await expect(page.getByTestId("event-log-item")).toContainText(
     "Spots for program items at 19:00 were randomized. Unfortunately, we couldn't fit you into any of your chosen program items.",
+  );
+});
+
+test("Receive spot in lottery signup, with multiple lottery program types", async ({
+  page,
+  request,
+}) => {
+  const startTime = dayjs(config.event().eventStartTime)
+    .add(4, "hour")
+    .startOf("hour")
+    .toISOString();
+  const workshopTitle = "test workshop";
+  const rpgTitle = "test rpg";
+
+  await populateDb(request, {
+    clean: true,
+    admin: true,
+  });
+  await addUser(request, "test1");
+  await addProgramItems(request, [
+    {
+      programItemId: testProgramItem.programItemId,
+      programType: ProgramType.WORKSHOP,
+      title: workshopTitle,
+      startTime,
+      // Adjust min/max so user can get the seat
+      minAttendance: 1,
+      maxAttendance: 1,
+    },
+    {
+      programItemId: testProgramItem2.programItemId,
+      programType: ProgramType.TABLETOP_RPG,
+      title: rpgTitle,
+      startTime,
+      // Adjust min/max so user can get the seat
+      minAttendance: 1,
+      maxAttendance: 1,
+    },
+  ]);
+
+  await postSettings(request, {
+    signupStrategy: EventSignupStrategy.LOTTERY_AND_DIRECT,
+  });
+
+  await postTestSettings(request, {
+    testTime: config.event().eventStartTime,
+  });
+
+  await login(page, request, { username: "test1", password: "test" });
+  await page.goto("/");
+
+  await page.click("data-testid=program-list-tab");
+
+  const workshopProgramItem = page
+    .locator('[data-testid="program-item-container"]')
+    .filter({
+      has: page.getByTestId("program-item-title"),
+      hasText: workshopTitle,
+    });
+  await workshopProgramItem
+    .getByRole("button", { name: /lottery sign-up/i })
+    .click();
+  await workshopProgramItem.getByRole("button", { name: /confirm/i }).click();
+
+  const rpgProgramItem = page
+    .locator('[data-testid="program-item-container"]')
+    .filter({
+      has: page.getByTestId("program-item-title"),
+      hasText: rpgTitle,
+    });
+  await rpgProgramItem
+    .getByRole("button", { name: /lottery sign-up/i })
+    .click();
+  await rpgProgramItem.getByRole("button", { name: /confirm/i }).click();
+
+  // Do assignment on background
+  await postAssignment(request, startTime);
+  await page.reload();
+
+  // Check new assigment message
+  await expect(page.getByTestId("notification-bar")).toContainText(
+    `You were assigned to the workshop ${workshopTitle}.`,
+  );
+
+  await page.getByRole("link", { name: "Show all notifications" }).click();
+  await expect(page.getByTestId("event-log-item")).toContainText(
+    `You were assigned to the workshop ${workshopTitle}.`,
   );
 });
