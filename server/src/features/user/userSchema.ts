eac29f6d151b24a99ae325ad69a7a8bbd512e5bb
Arttu Hanska <arttu.hanska@gmail.com>
2025-08-31 12:53:10 +0300

    Fix typo in user schema

--- server/src/features/user/userSchema.ts
+++ server/src/features/user/userSchema.ts
@@ -96,7 +96,7 @@ const userSchema = new mongoose.Schema(
     lotterySignups: { type: [lotterySignupSchema], required: true },
     eventLogItems: { type: [eventLogItemSchema], required: true },
     email: { type: String, required: false },
-    emailNotificationPermitAsked: { type: Boolean, requires: true },
+    emailNotificationPermitAsked: { type: Boolean, required: true },
     createdAt: {
       type: Date,
       get: (value: Date) => new Date(value),
