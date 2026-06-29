908862987f5c0680854dde12c6ee403b1d7f5d22
Maija Nevala <>
2026-06-29 17:46:47 +0300

    Add cancel to lottery sign-up item.

--- client/src/views/my-program-items/components/LotterySignupItem.tsx
+++ client/src/views/my-program-items/components/LotterySignupItem.tsx
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router";
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
+import { useAppDispatch } from "client/utils/hooks";
 import { LotterySignupWithProgramItem } from "client/views/my-program-items/myProgramItemsSlice";
+import {
+  DeleteLotterySignupErrorMessage,
+  submitDeleteLotterySignup,
+} from "client/views/my-program-items/myProgramItemsThunks";
 import {
   MyProgramButtonContainer,
   MyProgramGameTitle,
@@ -11,6 +16,8 @@ import {
 import { TertiaryButton } from "client/components/TertiaryButton";
 import { AppRoute } from "client/app/AppRoutes";
 import { PopularityInfo } from "client/components/PopularityInfo";
+import { CancelSignupForm } from "client/views/program-item/signup/components/CancelSignupForm";
+import { ErrorMessage } from "client/components/ErrorMessage";
 
 interface Props {
   lotterySignup: LotterySignupWithProgramItem;
@@ -19,6 +26,28 @@ interface Props {
 export const LotterySignupItem = ({ lotterySignup }: Props): ReactElement => {
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(false);
+  const [cancelSignupFormOpen, setCancelSignupFormOpen] = useState(false);
+  const [serverError, setServerError] =
+    useState<DeleteLotterySignupErrorMessage | null>(null);
+
+  const removeSignup = async (): Promise<void> => {
+    setLoading(true);
+    const errorMessage = await dispatch(
+      submitDeleteLotterySignup({
+        lotterySignupProgramItemId: lotterySignup.programItemId,
+      }),
+    );
+
+    if (errorMessage) {
+      setServerError(errorMessage);
+    } else {
+      setCancelSignupFormOpen(false);
+    }
+
+    setLoading(false);
+  };
 
   return (
     <MyProgramListItem>
@@ -33,18 +62,46 @@ export const LotterySignupItem = ({ lotterySignup }: Props): ReactElement => {
           programType={lotterySignup.programItem.programType}
         />
 
-        <StyledButtons>
-          <TertiaryButton
-            icon="circle-arrow-right"
-            onClick={async () => {
-              await navigate(
-                `${AppRoute.PROGRAM_ITEM}/${lotterySignup.programItemId}`,
-              );
-            }}
-          >
-            {t("button.showInfo")}
-          </TertiaryButton>
-        </StyledButtons>
+        {serverError && (
+          <StyledErrorMessage
+            message={t(serverError)}
+            closeError={() => setServerError(null)}
+          />
+        )}
+
+        {!cancelSignupFormOpen && (
+          <StyledButtons>
+            <TertiaryButton
+              icon="circle-arrow-right"
+              onClick={async () => {
+                await navigate(
+                  `${AppRoute.PROGRAM_ITEM}/${lotterySignup.programItemId}`,
+                );
+              }}
+            >
+              {t("button.showInfo")}
+            </TertiaryButton>
+            <TertiaryButton
+              icon="calendar-xmark"
+              onClick={() => setCancelSignupFormOpen(true)}
+            >
+              {t("button.cancelSignup")}
+            </TertiaryButton>
+          </StyledButtons>
+        )}
+
+        {cancelSignupFormOpen && (
+          <CancelSignupFormContainer>
+            <CancelSignupForm
+              onCancelForm={() => {
+                setServerError(null);
+                setCancelSignupFormOpen(false);
+              }}
+              onConfirmForm={async () => await removeSignup()}
+              loading={loading}
+            />
+          </CancelSignupFormContainer>
+        )}
       </Grid>
     </MyProgramListItem>
   );
@@ -63,13 +120,30 @@ const StyledTitle = styled(MyProgramGameTitle)`
 
 const StyledButtons = styled(MyProgramButtonContainer)`
   grid-column: 1;
+  grid-row: 3;
+`;
+
+const StyledErrorMessage = styled(ErrorMessage)`
+  grid-column: 1;
   grid-row: 2;
+  margin: 4px 0 8px 0;
+`;
+
+const CancelSignupFormContainer = styled.div`
+  grid-column: 1;
+  grid-row: 3;
+  display: flex;
+
+  @media (max-width: ${(props) => props.theme.breakpointPhone}) {
+    flex-direction: column;
+    margin-left: -16px;
+  }
 `;
 
 const StyledPopularityInfo = styled(PopularityInfo)`
   grid-column: 2;
   grid-row-start: 1;
-  grid-row-end: 3;
+  grid-row-end: 4;
   align-items: center;
   justify-content: center;
 
