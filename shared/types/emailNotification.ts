8e376d8cc4482a7bf2f03f61d0c096c9d8cfe068
user <user@example.com>
2026-06-29 08:05:55 +0300

    Send emails when sign-up type or starting time changes

--- shared/types/emailNotification.ts
+++ shared/types/emailNotification.ts
@@ -3,4 +3,7 @@ export enum EmailNotificationTrigger {
   REJECTED = "rejected",
   PROGRAM_ITEM_CANCELLED = "programItemCancelled",
   PROGRAM_ITEM_DELETED = "programItemDeleted",
+  PROGRAM_ITEM_NO_KONSTI_SIGNUP_ANYMORE = "programItemNoKonstiSignupAnymore",
+  PROGRAM_ITEM_NO_LOTTERY_ANYMORE = "programItemNoLotteryAnymore",
+  PROGRAM_ITEM_TIME_CHANGED = "programItemTimeChanged",
 }
