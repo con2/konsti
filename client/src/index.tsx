a41031a8817af84cb6c66d6a4b7965676afa8f1b
Arttu Hanska <arttu.hanska@gmail.com>
2026-07-17 00:07:38 +0300

    Sentry: ignore Instagram iOS browser error

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -84,6 +84,8 @@ init({
     // Error when Outlook scans a link
     // https://github.com/getsentry/sentry-javascript/issues/3440
     "Non-Error promise rejection captured with value: Object Not Found Matching Id:",
+    // Error from the script the Instagram iOS in-app browser injects into every page
+    "evaluating 'window.webkit.messageHandlers'",
   ],
   denyUrls: [
     // Errors from scripts the Facebook in-app browser injects into every page
